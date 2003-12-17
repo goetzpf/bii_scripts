8998b8f341ff2353c4ceb12967dc59e800aa794d
pfeiffer <unknown>
2003-12-17 16:10:16 +0000

    * primary_keys has now a different set of parameters!
    * primary_keys has now a different set of parameters!
      The table-name can now have the form "owner.table_name".
      The other way is to give the table-owner as an additional
      parameter. If the given table-name is a synonym, the
      table-name the synonym points to is determined and used for
      the SQL statement that determines the primary key.
    * foreign_keys() and resident_keys() have been changed
      in the same way like primary_keys().
    * a reverse synonym hash is now computed by get_synonyms(), too.

--- lib/perl/dbdrv_oci.pm
+++ lib/perl/dbdrv_oci.pm
@@ -7,24 +7,36 @@ my $mod_l= "dbdrv_oci";
 sub primary_keys
 # returns the one primary key or the list of columns
 # that form the primary key
-  { my($dbh,$table_name)= @_;
+  { my($dbh,$user_name,$table_name,$table_owner)= @_;
   
     $dbh= check_dbi_handle($dbh);
     return if (!defined $dbh);
 
     $table_name= uc($table_name);
+
+    if ($table_name =~ /\./)
+      { ($table_owner,$table_name)= split(/\./,$table_name); };
+      
+    if (!defined $table_owner)
+      { ($table_name,$table_owner)=
+                    dbdrv::real_name($dbh,$user_name,$table_name);
+      }; 
     
     my $SQL= "SELECT a.owner, a.table_name, b.column_name " .
              "FROM all_constraints a, all_cons_columns b " .
-	     "WHERE a.constraint_type='P' AND " .
-		  " a.constraint_name=b.constraint_name AND " .
-		  " a.table_name = \'$table_name\'";
+             "WHERE a.constraint_type='P' AND " .
+                  " a.constraint_name=b.constraint_name AND " .
+                  " a.table_name = \'$table_name\'";
+
+    # take table owner into account
+    if (defined $table_owner)
+      { $SQL.= " AND a.owner=\'$table_owner\'"; };
     
     sql_trace($SQL) if ($sql_trace);
     
     my $res=
       $dbh->selectall_arrayref($SQL);
-		      	   
+                           
     if (!defined $res)
       { dberror($mod_l,'db_get_primary_keys',__LINE__,
                 "selectall_arrayref failed, errcode:\n$DBI::errstr"); 
@@ -47,27 +59,44 @@ sub primary_keys
   }
 
 sub foreign_keys
-  { my($dbh,$table_name)= @_;
+  { my($dbh,$user_name,$table_name,$table_owner)= @_;
 
     $dbh= check_dbi_handle($dbh);
     return if (!defined $dbh);
 
     $table_name= uc($table_name);
-    my $SQL="select ST.TABLE_NAME, CL.COLUMN_NAME, " .
-                   "ST.CONSTRAINT_NAME, ST.R_CONSTRAINT_NAME, " .
-		   "CL2.TABLE_NAME AS FOREIGN_TABLE, " .
-		   "CL2.COLUMN_NAME AS FOREIGN_COLUMN, " .
-		   "ST.OWNER, ST2.OWNER " .
-		   "FROM " .
-		   "all_cons_columns CL, all_cons_columns CL2, " .
-		   "all_constraints ST, all_constraints ST2 " .
-		   "WHERE " .
-		   "ST.TABLE_NAME=\'$table_name\' AND ".
-		   "ST.CONSTRAINT_TYPE=\'R\' AND " .
-		   "ST.CONSTRAINT_NAME=CL.CONSTRAINT_NAME AND " .
-		   "ST.R_CONSTRAINT_NAME= ST2.CONSTRAINT_NAME AND " .
-		   "CL2.CONSTRAINT_NAME=ST2.CONSTRAINT_NAME AND ".
-		   "ST2.OWNER=CL2.OWNER";   
+    
+    if ($table_name =~ /\./)
+      { ($table_owner,$table_name)= split(/\./,$table_name); };
+    
+    if (!defined $table_owner)
+      { ($table_name,$table_owner)=
+                    dbdrv::real_name($dbh,$user_name,$table_name);
+      }; 
+
+    my $SQL= "select ST.TABLE_NAME, CL.COLUMN_NAME, " .
+                    "ST.CONSTRAINT_NAME, ST.R_CONSTRAINT_NAME, " .
+                    "CL2.TABLE_NAME AS FOREIGN_TABLE, " .
+                    "CL2.COLUMN_NAME AS FOREIGN_COLUMN, " .
+                    "ST.OWNER, ST2.OWNER " .
+             "FROM " .
+                    "all_cons_columns CL, all_cons_columns CL2, " .
+                    "all_constraints ST, all_constraints ST2 " .
+             "WHERE ";
+             
+             
+    if (defined $table_owner)
+      { $SQL.= "ST.OWNER=\'$table_owner\' AND "; };
+    
+    $SQL.=          "ST.TABLE_NAME=\'$table_name\' AND " .
+                    "ST.CONSTRAINT_TYPE='R' AND " .
+                    "ST.CONSTRAINT_NAME=CL.CONSTRAINT_NAME AND " .
+                    "ST.OWNER=CL.OWNER AND " .
+                    "ST.R_CONSTRAINT_NAME= ST2.CONSTRAINT_NAME AND " .
+                    "ST.R_OWNER=ST2.OWNER AND " .
+                    "ST2.CONSTRAINT_NAME=CL2.CONSTRAINT_NAME AND " .
+                    "ST2.OWNER=CL2.OWNER";   
+    
     sql_trace($SQL) if ($sql_trace);
     my $res=
       $dbh->selectall_arrayref($SQL);
@@ -78,8 +107,8 @@ sub foreign_keys
     # do only take lines where both OWNERS match
     # if you adde "ST.OWNER=ST2.OWNER" to the SQL statement
     # it takes 4 minutes instead of 1 second !!!
-    # perl is a bit faster... ;-)	
-		      	   
+    # perl is a bit faster... ;-)       
+                           
     if (!defined $res)
       { dberror($mod_l,'db_get_foreign_keys',__LINE__,
                 "selectall_arrayref failed, errcode:\n$DBI::errstr"); 
@@ -87,13 +116,18 @@ sub foreign_keys
       };
 
     # build a hash,
-    # col_name => [foreign_table,foreign_column]
+    # col_name => [foreign_table,foreign_column,foreign_table_owner]
     my %foreign_keys;
     foreach my $r_line (@$res)
       { # owner mismatch
-        next if ($r_line->[6] ne $r_line->[7]);
+#        next if ($r_line->[6] ne $r_line->[7]);
+
+# returns column-name -> FOREIGN_TABLE FOREIGN_COLUMN,FOREIGN_OWNER 
       
-        $foreign_keys{ $r_line->[1] } = [ $r_line->[4],$r_line->[5] ]; 
+        $foreign_keys{ $r_line->[1] } = [ $r_line->[4],
+                                          $r_line->[5], $r_line->[7] ]; 
+
+#warn "$r_line->[1] -> ( $r_line->[4],$r_line->[5] )";
       };
 
     return( \%foreign_keys);
@@ -103,54 +137,85 @@ sub foreign_keys
 sub resident_keys
 # the opposite of foreign keys,
 # find where the primary key of this table is used as foreign key
-  { my($dbh,$table_name)= @_;
+  { my($dbh,$user_name,$table_name,$table_owner)= @_;
 
     $dbh= check_dbi_handle($dbh);
     return if (!defined $dbh);
 
     $table_name= uc($table_name);
-    my $SQL= "select ST.TABLE_NAME, CL.COLUMN_NAME, ST.CONSTRAINT_NAME, " .
-		   " ST2.TABLE_NAME AS R_TABLE, " .
-		   " CL2.COLUMN_NAME AS R_COLUMN, " .
-		   " ST2.CONSTRAINT_NAME AS R_CONSTRAINT_NAME " .
-	     "from  " .
-	     "all_constraints ST, all_constraints ST2,  " .
-	     "all_cons_columns CL, all_cons_columns CL2 " .
-	     "where  ST.TABLE_NAME=\'$table_name\' AND " .
-		   " ST.CONSTRAINT_TYPE='P' AND " .
-		   " ST.CONSTRAINT_NAME=ST2.R_CONSTRAINT_NAME AND " .
-		   " ST2.CONSTRAINT_TYPE='R' AND " .
-		   " ST.CONSTRAINT_NAME=CL.CONSTRAINT_NAME AND " .
-		   " ST2.CONSTRAINT_NAME=CL2.CONSTRAINT_NAME ";
+    
+    if ($table_name =~ /\./)
+      { ($table_owner,$table_name)= split(/\./,$table_name); };
+
+    if (!defined $table_owner)
+      { ($table_name,$table_owner)=
+                    dbdrv::real_name($dbh,$user_name,$table_name);
+      }; 
+
+    my $SQL= "select ST.TABLE_NAME, ST.OWNER, CL.COLUMN_NAME, " .
+                   "ST.CONSTRAINT_NAME, " .
+                   " ST2.TABLE_NAME AS R_TABLE, " .
+                   " CL2.COLUMN_NAME AS R_COLUMN, " .
+                   " ST2.CONSTRAINT_NAME AS R_CONSTRAINT_NAME " .
+                   " ST2.OWNER AS R_OWNER " .
+             "from  " .
+             "all_constraints ST, all_constraints ST2,  " .
+             "all_cons_columns CL, all_cons_columns CL2 " .
+             "where ";
+             
+# Oracle is too slow for this:
+#    if (defined $table_owner)
+#      { $SQL.=      "ST.OWNER=\'$table_owner\' AND "; };
+
+
+    $SQL.=         " ST.TABLE_NAME=\'$table_name\' AND " .
+                   " ST.CONSTRAINT_TYPE='P' AND " .
+                   " ST.CONSTRAINT_NAME=ST2.R_CONSTRAINT_NAME AND " .
+                   " ST2.CONSTRAINT_TYPE='R' AND " .
+                   " ST.CONSTRAINT_NAME=CL.CONSTRAINT_NAME AND " .
+                   " ST2.CONSTRAINT_NAME=CL2.CONSTRAINT_NAME ";
 
     sql_trace($SQL) if ($sql_trace);
-    my $res=
+    my $res_r=
       $dbh->selectall_arrayref($SQL);
     # gives:
-    # TABLE_NAME COLUMN_NAME CONSTRAINT_NAME R_TABLE R_COLUMN R_CONSTRAINT_NAME 
-	
-    if (!defined $res)
+    # TABLE_NAME TABLE_OWNER COLUMN_NAME CONSTRAINT_NAME 
+    #  R_TABLE R_COLUMN R_CONSTRAINT_NAME R_OWNER
+        
+    if (!defined $res_r)
       { dberror($mod_l,'db_get_resident_keys',__LINE__,
                 "selectall_arrayref failed, errcode:\n$DBI::errstr"); 
         return;
       };
 
     # build a hash,
-    # col_name => [ [resident_table1,resident_column1],
-    #               [resident_table2,resident_column2], 
+    # col_name => [ [resident_table1,resident_column1,resident_owner],
+    #               [resident_table2,resident_column2,resident_owner], 
     #                    ...
     #             ]
     #      resident_table may occur more than once
-    if ($#$res < 0)
+    
+    my @res;
+    if (defined $table_owner)
+      { foreach my $r_line (@$res_r)
+          { next if ($r_line->[1] ne $table_owner); 
+            push @res, $r_line;
+          }
+      }
+    else
+      { @res= @$res_r; };
+
+
+    if ($#res < 0)
       { # no resident keys found, just return "undef"
         return;
       };
       
     my %resident_keys;
-    foreach my $r_line (@$res)
+    foreach my $r_line (@res)
       { 
-        push @{ $resident_keys{ $r_line->[1] } },
-	         [ $r_line->[3],$r_line->[4] ]; 
+        push @{ $resident_keys{ $r_line->[2] } },
+                 [ $r_line->[4],$r_line->[5], $r_line->[7] ]; 
       };
 
     return( \%resident_keys);
@@ -219,7 +284,8 @@ sub get_synonyms
 # type is 'T' (table) or 'V' (view)
 # $t_name: table or view referred to
 # $t_own: owner of referred table or view
-  { my($dbh,$r_syn)= @_;
+# $r_reverse_syn : a hash: owner.table => synonym
+  { my($dbh,$r_syn,$r_reverse_syn)= @_;
     
     die if (!defined $r_syn);
     die if (ref($r_syn) ne 'HASH');
@@ -236,7 +302,7 @@ sub get_synonyms
                    "asyn.table_owner NOT IN ('SYS', 'SYSTEM') AND " .
                    "asyn.table_name=at.table_name AND " . 
                    "asyn.table_owner=at.owner" ;
-		      
+                      
     sql_trace($sql) if ($sql_trace);
 
     my $res= $dbh->selectall_arrayref($sql);
@@ -250,7 +316,10 @@ sub get_synonyms
     # hash: [type('T'or'V'),owner,table-name,table-owner
     foreach my $line (@$res)
       { $r_syn->{ $line->[0] } = 
-                    ['T',$line->[1], $line->[2], $line->[3] ]; };
+                    ['T',$line->[1], $line->[2], $line->[3] ]; 
+                    
+        $r_reverse_syn->{$line->[3] . '.' . $line->[2]}= $line->[0]; 
+      };
 
 
     $sql= "SELECT asyn.synonym_name,asyn.owner, " .
@@ -260,7 +329,7 @@ sub get_synonyms
                    "asyn.table_owner NOT IN ('SYS', 'SYSTEM') AND " .
                    "asyn.table_name=av.view_name AND " . 
                    "asyn.table_owner=av.owner" ;
-		      
+                      
     sql_trace($sql) if ($sql_trace);
 
     my $res= $dbh->selectall_arrayref($sql);
@@ -271,10 +340,13 @@ sub get_synonyms
         return;
       };
 
-    # hash: [type('T'or'V'),owner,table-name,table-owner
+    # hash: [type('T'or'V'),synonym-owner,table-name,table-owner
     foreach my $line (@$res)
       { $r_syn->{ $line->[0] } 
-                 = ['V',$line->[1], $line->[2], $line->[3] ]; };
+                 = ['V',$line->[1], $line->[2], $line->[3] ]; 
+
+        $r_reverse_syn->{$line->[3] . '.' . $line->[2]}= $line->[0]; 
+      };
 
     #print Dumper($r_syn);
     return(1);
