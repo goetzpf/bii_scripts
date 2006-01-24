68b0bda20dbbf133ef3600f8dbc18b3dc0b5f662
pfeiffer <unknown>
2006-01-24 12:38:18 +0000

    scripts are now installed directly to /opt/csr/bin
    scripts are now installed directly to /opt/csr/bin
    (when "./install" is used)

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 cvs -q update
-perl Makefile.PL prefix=/opt/csr/lib/perl USE_PERL5LIB=1 SHARE_DIR=/opt/csr/share
+perl Makefile.PL prefix=/opt/csr/lib/perl INSTALLSCRIPT=/opt/csr/bin USE_PERL5LIB=1 SHARE_DIR=/opt/csr/share
 make install
