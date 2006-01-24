ade3bb8f09cbf4280f16bbdd3d009f08b5fa177c
pfeiffer <unknown>
2006-01-24 13:24:14 +0000

    the paths are now better set

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 cvs -q update
-perl Makefile.PL prefix=/opt/csr/lib/perl INSTALLSCRIPT=/opt/csr/bin USE_PERL5LIB=1 SHARE_DIR=/opt/csr/share
+perl Makefile.PL prefix=/opt/csr lib=/opt/csr/lib/perl \
+INSTALLSITEMAN1DIR=/opt/csr/share/man/man1 INSTALLSITEMAN3DIR=/opt/csr/share/man/man3 \
+USE_PERL5LIB=1 SHARE_DIR=/opt/csr/share
 make install
