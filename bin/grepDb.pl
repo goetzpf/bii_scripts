4037ef4ce4695f75096ecc3984c124b2ec83dfe4
kuner <unknown>
2006-02-28 14:06:31 +0000

    grepDb.pl: Serach in EPICS db files

---
+++ bin/grepDb.pl
@@ -0,0 +1,116 @@
+eval 'exec perl -S $0 ${1+"$@"}'  # -*- Mode: perl -*-
+    if $running_under_some_shell;
+
+## grepDb.pl:
+# *****************
+#
+# search in an EPICS db file. 
+#
+#  *  USAGE: grepDb.pl -t<TRIGGER> <match> -p<Print> <match> filename
+#
+#  Trigger options may be set al gusto, ar processed as regular expressions 
+#  concatenated with AND:
+#
+#      -tt <recType>: record type
+#      -tr <recName>: record name
+#      -tf <fieldType>: field type
+#      -tc <cont>:    field as defined with -f contains <cont>
+#
+#  Print options: default: all: record and fields
+#
+#      -pf <fieldType>: print this field/s
+#      -pt <recType>:   print records of this type
+#      -pr <recName>:   print records tha match that name
+#
+#  *  Example  :
+#
+#        grepDb.pl -t bo -r PHA1R -f DTYP -c lowcal -pr '' -pf '(DTYP|OUT)' filename
+#
+    use strict;
+    use Getopt::Long;
+    use parse_db;
+    use Data::Dumper;
+
+    my $trigRecType = ".";
+    my $trigRecName = ".";
+    my $trigFieldName = ".";
+    my $trigFieldValue = ".";
+    my $prRecType = ".";
+    my $prRecName = ".";
+    my $prFieldName = ".";
+
+    die unless GetOptions("tt=s"=>\$trigRecType, "tr=s"=>\$trigRecName, "tf=s"=>\$trigFieldName, "tc=s"=>\$trigFieldValue,
+                           "pt"=>\$prRecType, "pn=s"=>\$prRecName, "pf=s"=>\$prFieldName);
+
+    my( $filename ) = shift @ARGV;  # may be a filename OR a commandline gadget!
+    my $file;
+    open(IN_FILE, "<$filename") or die "can't open input file: $filename";
+    { local $/;
+      undef $/; # ignoriere /n in der Datei als Trenner
+      $file = <IN_FILE>;
+    }  
+    close IN_FILE;
+
+# parse db
+    my $rH_recName2recType;
+    my $rH_recType2recName;
+    my $rH_records;
+    while($file =~ /\G\s*record\s*\((\w*)\s*,\s*\"([^\"]*)\"\)[\s\r\n]*\{\n(.*?)\}/gsc)
+    {
+        $file = $';
+
+#print "$1,$2 is\n$3\n\n";
+        my $recordType = $1;
+        my $recordName = $2;
+        $rH_recName2recType->{$recordName} = $recordType;
+        $rH_recType2recName->{$recordType}->{$recordName} = 1;
+        
+        my @allFields = split("\n",$3);
+        my $rH_thisFields;
+        foreach my $fieldLine (@allFields)
+        {
+            if( $fieldLine =~ /\G[\s\r\n]*field\s*\(\s*(\w*)\s*,\s*\"([^\"]*)\"\)/gsc )
+            {
+                my($field,$value)= ($1,$2);
+                $rH_thisFields->{$field} = $value;
+            }
+            else
+            {
+                warn "illegal Field definition \'$fieldLine\' in record \'$recordName\'";
+            }
+	}
+        $rH_records->{$recordName} = $rH_thisFields;
+    }
+#    print Dumper($rH_records);
+#    print Dumper($rH_recName2recType);
+#    print Dumper($rH_recType2recName);
+
+# process trigger options
+    foreach my $record (keys(%$rH_records))
+    {
+        my $recT = $rH_recName2recType->{$record} ;
+
+        my $recordFlag;
+        if( $record =~ /$trigRecName/ && $recT =~ /$trigRecType/ )
+        {
+            foreach my $field ( keys( %{$rH_records->{$record}} ) )
+            {
+                my $fVal = $rH_records->{$record}->{$field};
+
+                if( $field =~ /$trigFieldName/ && $fVal =~ /$trigFieldValue/ )
+                {
+# process print options
+                    unless( defined $recordFlag && $record =~ /$prRecName/ && $recT =~ /$prRecType/ )
+                    {
+                        print "record($recT,\"$record\")  {\n";
+                        $recordFlag = 1;
+                    }
+                    if( $field =~ /$prFieldName/ )
+                    {   
+                        print "\tfield($field,\"$fVal\")\n";
+                    }
+                }
+            }
+        print "}\n" if defined $recordFlag ;
+        }
+    }
