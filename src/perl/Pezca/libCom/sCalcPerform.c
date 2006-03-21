bc1c8e15c8c69941f8b0a2a4a7a8c56f01a443b2
pfeiffer <unknown>
2006-03-21 14:17:30 +0000

    a small change in order to be able to compile
    a small change in order to be able to compile
    sCalcPerform.c under Linux...

--- src/perl/Pezca/libCom/sCalcPerform.c
+++ src/perl/Pezca/libCom/sCalcPerform.c
@@ -76,6 +76,7 @@
 
 static double	local_random();
 
+#ifndef linux
 static int isnan(double d)
 {
 	union { long l[2]; double d; } u;
@@ -84,6 +85,8 @@ static int isnan(double d)
 	if (u.l[1] || (u.l[0] & 0x000fffff)) return(1);
 	return(0);
 }
+#endif
+
 #define myNINT(a) ((int)((a) >= 0 ? (a)+0.5 : (a)-0.5))
 #ifndef PI
 #define PI 3.141592654
