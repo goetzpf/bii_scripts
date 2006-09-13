cdbc333d33bdbf742655403e6872127a663e0535
pfeiffer <unknown>
2006-09-13 11:00:23 +0000

    two utilties for CVS...

---
+++ bin/cvs_diff.pl
@@ -0,0 +1,285 @@
+eval 'exec perl -S $0 ${1+"$@"}' # -*- Mode: perl -*-
+    if 0;
+# the above is a more portable way to find perl
+# ! /usr/bin/perl
+
+# @STATUS: release
+# @PLATFORM: home bessy
+# @CATEGORY: search
+
+
+# [cvs_diff.pl] -- compare c-files against the repository
+
+use strict;
+
+use FindBin;
+use Getopt::Long;
+use Config;
+use File::Spec;
+use Text::Tabs;
+
+use vars qw($opt_help 
+            $opt_no_comments $opt_no_multiple_empty_lines
+            $opt_summary $opt_revision $opt_revision2 $opt_type);
+
+
+my $sc_version= "0.9";
+
+my $sc_name= $FindBin::Script;
+my $sc_summary= "compare c-files against the repository"; 
+my $sc_author= "Goetz Pfeiffer";
+my $sc_year= "2006";
+
+my %type= (c => 1,
+           any => 2);
+	    
+my $type= 'c';	    
+
+my $debug= 1; # global debug-switch
+
+my $tmpdir= "/tmp";
+
+
+#Getopt::Long::config(qw(no_ignore_case));
+
+if (!GetOptions("help|h","summary",
+                "revision|r=s","revision2|s=s",
+		"no_comments|c",
+		"no_multiple_empty_lines|m",
+		"type|t=s"
+	
+                ))
+  { die "parameter error!\n"; };
+
+if ($opt_help)
+  { help();
+    exit;
+  };
+
+if ($opt_summary)
+  { print_summary();
+    exit;
+  };
+
+if (defined $opt_type)
+  { if (!exists $type{$opt_type})
+      { die "unknown type: $opt_type"; };
+    $type= $opt_type;
+  };
+
+# ------------------------------------------------
+
+my $filename= shift(@ARGV);
+
+if (!defined $filename)
+  { die "error: name of the file is missing!\n"; };
+
+print "file $filename:\ncomparing ";
+
+my $file1= process($filename,$opt_revision,1,
+                   $opt_no_comments,$opt_no_multiple_empty_lines);
+
+if (!defined $file1)
+  { die; };
+
+print " against ";
+
+$opt_revision2= "local" if (!defined $opt_revision2);
+my $file2= process($filename,$opt_revision2,2,
+                   $opt_no_comments,$opt_no_multiple_empty_lines);
+
+if (!defined $file2)
+  { unlink($file1);  
+    die; 
+  };
+
+print "\n";
+
+show_diff($file1,$file2);
+unlink($file1);
+unlink($file2);
+
+exit(0);
+
+# fit in program text here
+
+sub basename
+  { my($path)= @_;
+    
+    my ($volume,$directories,$file) = File::Spec->splitpath( $path );
+    
+    if ($type eq 'c')
+      { if ($file!~ /(.*)\.(c|cc|cpp|CC)$/)
+          { die "error: file $file is not a c file\n"; };
+      };
+      
+    return($1);
+  }
+
+sub process
+  { my($filename,$rev,$no,$rm_comments,$rm_spc_lines)= @_;
+  
+    if ($rev eq "local")
+      { print "working copy";
+        return(local_process($filename,$no,$rm_comments,$rm_spc_lines)); 
+      }
+    else
+      { my $opt;
+        if (!defined $rev)
+          { print "trunk"; }
+	else
+	  { print "revision $rev";
+	    $opt= "-r $rev"; 
+	  };
+        return(cvs_process($filename,$opt,$no,$rm_comments,$rm_spc_lines)); 
+      }
+  }
+
+sub slurp
+  { my($filename)= @_;
+    local(*F);
+    local($/);
+    undef $/;
+    open(F, $filename) or die "unable to open $filename";
+    
+    
+    my $content= <F>;
+    close(F);
+    return(\$content);
+  }
+  
+sub unslurp  
+  { my($filename,$r_content)= @_;
+  
+    local(*F);
+    open(F, ">$filename") or die "unable to create $filename";
+    print F $$r_content;
+    close(F);
+  }
+    
+  
+sub rm_comments
+  { my($r_content)= @_;
+   
+    if ($type eq 'c')
+      { # handle "<c-commands> // comment"
+	$$r_content=~ s/^(\S+?)\/\/(.*?)/$1/gm;
+
+	# handle " // comment"
+	$$r_content=~ s/^\s*\/\/(.*?)\n//gm;
+
+	# handle " /*  comment  */"
+	$$r_content=~ s/\/\*.*?\*\///g;
+      };
+  }
+  
+sub rm_mult_spc_lines  
+  { my($r_content)= @_;
+   
+    $$r_content=~ s/^\s+$//gm;
+    $$r_content=~ s/\n+/\n/g;
+  }
+    
+  
+sub expand_it
+  { my($r_content)= @_;
+  
+    my @lines= split(/\n/,$$r_content);
+    
+    for(my $i=0; $i<= $#lines; $i++)
+      { $lines[$i]= expand($lines[$i]); };
+    
+    $$r_content= join("\n",@lines);
+  }   
+    
+
+sub local_process
+  { my($filename,$no,$rm_comments,$rm_spc_lines)= @_;
+    my $cmd;
+  
+    my $base= basename($filename) . ".$no.db";
+    $base= File::Spec->catfile($tmpdir,$base);
+    
+    my $r= slurp($filename);
+    rm_comments($r) if ($rm_comments);
+    rm_mult_spc_lines($r) if ($rm_spc_lines);
+    expand_it($r);
+    unslurp($base,$r);
+    
+    return($base);
+  }
+    
+sub cvs_process
+  { my($filename,$cvs_options,$no,$rm_comments,$rm_spc_lines)= @_;
+    my $cmd;
+  
+    my $base= basename($filename) . ".$no.db";
+    $base= File::Spec->catfile($tmpdir,$base);
+    
+    if (!sys("cvs update $cvs_options -p $filename 2> /dev/null > " .
+             "$base"))
+      { return; };
+    my $r= slurp($base);
+    rm_comments($r) if ($rm_comments);
+    rm_mult_spc_lines($r) if ($rm_spc_lines);
+    expand_it($r);
+    unslurp($base,$r);
+      
+    return($base);
+  }
+ 
+sub show_diff
+  { my($f1,$f2)= @_;
+  
+    my $cmd= "tkdiff $f1 $f2 2> /dev/null";
+    sys($cmd,1);
+  }
+        
+sub sys
+  { my($cmd,$nowarn)= @_;
+  
+    print "$cmd\n" if ($debug);
+    if (system($cmd) && !$nowarn)
+      { warn "\"$cmd\" failed : $?"; 
+        return;
+      };
+    return(1);
+  }
+
+# ------------------------------------------------
+
+sub print_summary
+  { printf("%-20s: $sc_summary\n",
+           $sc_name);
+  }
+
+sub h_center
+  { my($st)= @_;
+    return( (' ' x (38 - length($st)/2)) . $st );
+  }
+
+sub help
+  { my $l1= h_center("**** $sc_name $sc_version -- $sc_summary ****");
+    my $l2= h_center("$sc_author $sc_year");
+    my $types= join(" ",keys %type);
+    print <<END;
+
+$l1
+$l2
+
+Syntax:
+  $sc_name {options} [file]
+
+  options:
+    -h: help
+    --summary: give a summary of the script
+    -r [cvs-revision] (mandatory)
+    -s [2nd cvs revision] (optional)
+    -c --no_comments
+    -m --no_multiple_empty_lines
+    --type [type] :
+      known types $types 
+      
+END
+  }
+
---
+++ bin/cvs_log_diff.pl
@@ -0,0 +1,405 @@
+eval 'exec perl -S $0 ${1+"$@"}' # -*- Mode: perl -*-
+    if 0;
+# the above is a more portable way to find perl
+# ! /usr/bin/perl
+
+# @STATUS: release
+# @PLATFORM: home bessy
+# @CATEGORY: search
+
+
+# [cvs_diff.pl] -- compare logs between two cvs revisions
+
+use strict;
+
+use FindBin;
+use Data::Dumper;
+
+use Getopt::Long;
+use Config;
+use File::Spec;
+use Text::Tabs;
+
+use vars qw($opt_help $opt_file
+            $opt_summary $opt_revision $opt_revision2 $opt_list
+	    $opt_taglist $opt_quiet $opt_branches);
+
+
+my $sc_version= "0.9";
+
+my $sc_name= $FindBin::Script;
+my $sc_summary= "compare logs between two cvs revisions"; 
+my $sc_author= "Goetz Pfeiffer";
+my $sc_year= "2006";
+
+my $debug= 1; # global debug-switch
+
+my $tmpdir= "/tmp";
+
+
+#Getopt::Long::config(qw(no_ignore_case));
+
+if (!GetOptions("help|h","summary", "file|f=s",
+                "revision|r=s","revision2|s=s",
+		"list|l", "taglist:i", "quiet|q",
+		"branches|b"
+                ))
+  { die "parameter error!\n"; };
+
+if ($opt_help)
+  { help();
+    exit;
+  };
+
+if ($opt_summary)
+  { print_summary();
+    exit;
+  };
+
+# ------------------------------------------------
+
+if ((!defined $opt_taglist) && (!defined $opt_revision))
+  { die "-r is mandatory"; };
+
+if (defined $opt_list)
+  { if (!defined $opt_revision2)
+      { die "-s is mandatory when -l is used"; };
+  }  
+
+my($r_files,$r_logs)= cvs_log();
+#print Dumper $r_logs;
+
+if (defined $opt_taglist)
+  { cvs_taglist($r_files,$r_logs,$opt_taglist);
+    exit(0);
+  };
+
+if (defined $opt_list)
+  { cvs_tag_change_display($r_files,$r_logs,$opt_revision,$opt_revision2);
+    exit(0);
+  };
+
+
+my @files;
+if (!defined $opt_file)
+  { @files= @$r_files; }
+else
+  { @files= ($opt_file); };
+
+foreach my $file (@files)
+  { cvs_log_display($r_logs,$file,
+                    $opt_revision,$opt_revision2,$opt_branches); }
+
+exit(0);
+
+
+my($r_files,$r_taginfo)= cvs_tags();
+
+foreach my $f (@$r_files)
+  { my $rep= cvs_logs($r_taginfo->{$f},$f,$opt_revision,$opt_revision2);
+    print $$rep,"\n"; 
+  }
+
+ 
+sub cvs_log
+  { local(*STATUS);
+    my $file;
+    my $lineno= 0;
+    
+    my %h;
+    my $r_file;
+    my $r_tags;
+    my $r_desc;
+    
+    my $desc_rev;
+    my $r_desc_text;
+    my @files;
+    
+  
+    open(STATUS, "cvs log 2>&1 |")
+      || die "can't fork: $!";
+    my $status= 'initial';
+    while (my $line=<STATUS>) 
+      { $lineno++;
+        if ($status eq 'initial')
+          { next if ($line=~ /^\s*$/);
+            if ($line=~/^\s*Working file:\s+(.*)$/)
+	      { $file= $1;
+	        $status= 'file';
+		$r_file= {};
+		$h{$file}= $r_file;
+		push @files,$file;
+	        next;
+              };
+	    next; # maybe a warning here ?  
+	  };
+
+	if ($status eq 'symbolic names')
+          { 
+#die "line: $line";
+	    if ($line=~ /^\s*([^:]+):\s+([\d\.]+)/)
+	      { 
+#die;
+	        $r_tags->{$1}= $2;
+	        next;
+	      };
+	    $status= 'file';  
+	  }
+
+	if ($status eq 'description')
+          { if ($line=~ /^-----/)
+	      { next; };
+	    if ($line=~ /^revision\s+([\d\.]+)/)
+	      { $desc_rev= $1;
+	        my $t= "";
+	        $r_desc_text= \$t;
+	        $r_desc->{$desc_rev}= $r_desc_text;
+		# $r_desc->{$rev}  
+	        $status= 'desc-text';
+		next;
+              };
+	    die "parse error in line $lineno:\n$line\n ";  
+	  }      
+
+	if ($status eq 'desc-text')
+	  { if ($line=~ /^-----/)
+	      { $status= 'description';
+	        next;
+	      };
+	    if ($line=~ /^====/)  
+	      { $status= 'initial';
+	        next;
+              };
+	    $$r_desc_text.= $line;
+	    next;
+	  }
+
+	if ($status eq 'file')
+	  { next if ($line=~ /^\s*$/);
+            if ($line=~ /^symbolic names:/)
+	      { $status= 'symbolic names';
+	        $r_tags= {};
+		$r_file->{TAGS}= $r_tags;
+	        next;
+	      };
+	    if ($line=~ /^description:/)
+	      { $status= 'description';
+	        $r_desc= {};
+		$r_file->{DESC}= $r_desc;
+	        next;
+	      };
+            if ($line=~ /^====/)  
+	      { $status= 'initial';
+	        next;
+              };
+   	    next;
+	  }  
+      } # while
+      	      
+    close STATUS || die "bad netstat: $! $?";
+    return(\@files,\%h);
+  }
+ 
+sub sortcmp_versions
+  { cmp_versions($a,$b); }
+
+sub cmp_versions
+  { my($a,$b)= @_;
+    my @x= split(/\./,$a);
+    my @y= split(/\./,$b);
+    my $x;
+    my $y;
+    for(;;)
+      { $x= shift @x;
+        $y= shift @y;
+	if (!defined $x)
+	  { return 0 if (!defined $y);
+	    return -1;
+	  };
+	if (!defined $y)
+	  { return 1; };
+	if ($x<$y)
+	  { return -1; };
+	if ($x>$y)
+	  { return 1; };
+      }
+  }	    
+      
+sub version_dots
+  { my($v)= @_;
+    my $w= $v;
+    $w=~ s/\.//g;
+    return(length($v)-length($w));
+  }      
+  
+sub cvs_log_display
+  { my($r_logs,$file,$from_tag,$to_tag,$use_branches)= @_;
+    my $dots;
+  
+    my $r_file_logs= $r_logs->{$file};
+    
+    if (!defined $r_file_logs)
+      { die "no log information about \"$file\"\n "; };  
+            
+    my $r_tags= $r_file_logs->{TAGS};
+    my $r_desc= $r_file_logs->{DESC};
+
+    my $from_rev= $r_tags->{$from_tag};
+    
+    if (!defined $from_rev)
+      { warn "tag $from_tag not known for file $file\n" if (!$opt_quiet);
+	return;
+      };
+ 
+    $dots= version_dots($from_rev);
+ 
+#die "from: $from_rev"; 
+    
+    my $to_rev;
+    if (defined $to_tag)
+      { $to_rev= $r_tags->{$to_tag};
+        if (!defined $to_rev)
+          { warn "tag $to_tag not known for file $file\n" if (!$opt_quiet);
+	    return;
+          };
+	my $x= version_dots($to_rev);
+	if ($x>$dots)
+	  { $dots= $x; };
+      };
+      
+    my @revs= sort sortcmp_versions (keys %$r_desc);
+    
+#print "$file FROM: $from_rev\n"; 
+    my $first=1;
+    for(my $i=0; $i<=$#revs; $i++)
+      { 
+ 
+#print "REV: $revs[$i]\n";      
+        next if (cmp_versions($revs[$i],$from_rev)<=0);
+        if (defined $to_rev)
+	  { next if (cmp_versions($to_rev,$revs[$i])<0); 
+	  
+	  };
+#warn;
+        if (!$use_branches)
+	  { next if (version_dots($revs[$i])>$dots); };
+
+	if ($first)
+	  { $first=0; 
+	    print "=" x 60,"\n";
+	    print "file $file:\n\n";
+	  }
+	else
+	  { print "-" x 30,"\n"; 
+	  };
+
+	print "revision $revs[$i]\n";
+	print ${$r_desc->{$revs[$i]}}; 
+      };
+  }
+
+sub cvs_tag_change_display
+  { my($r_files,$r_logs,$from_tag,$to_tag)= @_;
+  
+    foreach my $file (@$r_files)
+      { my $r_file_logs= $r_logs->{$file};
+    
+	if (!defined $r_file_logs)
+	  { die "no log information about \"$file\"\n "; };  
+            
+        my $r_tags= $r_file_logs->{TAGS};
+
+        my $from_rev= $r_tags->{$from_tag};
+	if (!defined $from_rev)
+	  { warn "tag $from_tag not known for file $file\n" if (!$opt_quiet);
+	    next;
+	  };
+
+        my $to_rev= $r_tags->{$to_tag};
+	if (!defined $to_rev)
+	  { warn "tag $to_tag not known for file $file\n" if (!$opt_quiet);
+	    next;
+	  };
+	
+	next if ($from_rev eq $to_rev);
+	
+	print $file,"\n";
+      } 
+  }	
+
+sub cvs_taglist
+  { my($r_files,$r_logs,$min_no)= @_;
+    my %tags;
+  
+    if ($min_no<=0)
+      { $min_no= $#$r_files + $min_no + 1; };
+    foreach my $file (@$r_files)
+      { my $r_file_logs= $r_logs->{$file};
+    
+	if (!defined $r_file_logs)
+	  { die "no log information about \"$file\"\n "; };  
+            
+        my $r_tags= $r_file_logs->{TAGS};
+	foreach my $t (keys %$r_tags)
+	  { $tags{$t}+=1; };
+
+      } 
+#print Dumper \%tags;
+#print "min:$min_no\n";  
+#die;
+    my @found= grep { $tags{$_}>=$min_no } (keys %tags);  
+      
+    print join("\n",(sort @found)),"\n"; 
+  }	
+	
+sub sys
+  { my($cmd,$nowarn)= @_;
+  
+    print "$cmd\n" if ($debug);
+    if (system($cmd) && !$nowarn)
+      { warn "\"$cmd\" failed : $?"; 
+        return;
+      };
+    return(1);
+  }
+
+# ------------------------------------------------
+
+sub print_summary
+  { printf("%-20s: $sc_summary\n",
+           $sc_name);
+  }
+
+sub h_center
+  { my($st)= @_;
+    return( (' ' x (38 - length($st)/2)) . $st );
+  }
+
+sub help
+  { my $l1= h_center("**** $sc_name $sc_version -- $sc_summary ****");
+    my $l2= h_center("$sc_author $sc_year");
+    print <<END;
+
+$l1
+$l2
+
+Syntax:
+  $sc_name {options} [file]
+
+  options:
+    -h: help
+    --summary: give a summary of the script
+    -r --revision  [cvs-tag1] (mandatory)
+    -s --revision2 [cvs-tag2] (optional)
+    -s [2nd cvs revision] (optional)
+    -f [file] show logs only for a single file
+    -l list only files that changed from tag1 to tag2
+    --taglist [no] 
+      no positive: list all tags found in at least [no] files
+      no negative: list all tags found in at least [fileno+no] files
+        e.g -taglist -2 with 100 files: must be found in at least 98 files
+    -q : suppress "tag [TAG] not known..." warnings	
+    -b --branches: include branches into log-display   
+END
+  }
+
