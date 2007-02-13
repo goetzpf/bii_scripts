9438f8c4d0c6072eae0728e9f6caf3a94672c3d8
laux <unknown>
2007-02-13 12:21:56 +0000

    Completly documented as per-pod
    Completly documented as per-pod
    Added 3 function:
     - verbose for setting verbosity
     - silent to disable verbosity
     - col_aliases for building aliased column striing for sel

--- lib/perl/ODB.pm
+++ lib/perl/ODB.pm
@@ -10,16 +10,22 @@ use Options;
 use strict;
 
 our $dbh;
-my $config;
+my $config={'verbose'=>0};
 
+#------------------------------------------------------------------------------
+# login - connect
+#------------------------------------------------------------------------------
 sub login {
   $config = shift;
   return $dbh if defined $dbh;
   $dbh = DBI->connect("dbi:Oracle:".$config->{"dbase"}, $config->{"user"}, $config->{"passwd"},
-    {RaiseError => 1, PrintError => 0, AutoCommit => 0, ShowErrorStatement => 1});
+    {RaiseError => 1, PrintError => 0, AutoCommit => 0, ShowErrorStatement => $config->{'verbose'}});
   return $dbh;
 }
 
+#------------------------------------------------------------------------------
+# logoff - disconnect
+#------------------------------------------------------------------------------
 sub logoff {
   if (defined $dbh) {
     $dbh->rollback if $config->{"not"};
@@ -27,10 +33,30 @@ sub logoff {
   }
 }
 
+#------------------------------------------------------------------------------
+# verbose - more output/debug
+#------------------------------------------------------------------------------
+sub verbose {
+ $config->{'verbose'} = 1;
+}
+
+#------------------------------------------------------------------------------
+# logoff - lessoutput/debug
+#------------------------------------------------------------------------------
+sub silent {
+ $config->{'verbose'} = 0;
+}
+
+#------------------------------------------------------------------------------
+# END - abort
+#------------------------------------------------------------------------------
 END {
   logoff;
 }
 
+#------------------------------------------------------------------------------
+# ins - insert rows
+#------------------------------------------------------------------------------
 sub ins {
   my ($table, %row) = @_;
   my $sql = "insert into $table (".join(",",keys(%row)).") values (".join(",",map("'$_'", values(%row))).")";
@@ -38,6 +64,43 @@ sub ins {
   $dbh->do($sql);
 }
 
+#------------------------------------------------------------------------------
+# del - delete rows
+#------------------------------------------------------------------------------
+sub del {
+  my ($table, $keycolumn, @keys) = @_;
+  if (length($keycolumn) > 0) {
+  	my $sql = "delete from $table where $keycolumn in (".join(",", @keys).")";
+  	Options::print_out("$sql;\n") if $config->{"verbose"};
+  	$dbh->do($sql);
+  }
+}
+
+#------------------------------------------------------------------------------
+# selone - select dataset
+#------------------------------------------------------------------------------
+sub col_aliases {
+	my ($r_columns, $r_aliases) = shift;
+	my $colstr = "*";
+	if ($#$r_columns > 0) {
+		$colstr = "";
+		for (my $colindex = 0; $colindex <= $#$r_columns; $colindex++) {
+			if (length($$r_aliases[$colindex]) > 0) {
+				$colstr .= sprintf("%s \"%s\",", $$r_columns[$colindex], $$r_aliases[$colindex]) ;
+			} else {
+				$colstr .= $$r_columns[$colindex]."," ;
+			}
+		# erase empty aliases
+		}
+		$colstr = substr($colstr, 0, -1);
+	}
+	Options::print_out("columns=($colstr);\n") if $config->{"verbose"};
+	return $colstr;
+}
+
+#------------------------------------------------------------------------------
+# sel_one - select dataset
+#------------------------------------------------------------------------------
 sub sel_one {
   my $result = sel(@_);
   if (@$result == 1) {
@@ -48,6 +111,9 @@ sub sel_one {
   }
 }
 
+#------------------------------------------------------------------------------
+# sel - select rows
+#------------------------------------------------------------------------------
 sub sel {
   my ($table, $col_names, $cond, @bind_values) = @_;
   $cond = " where $cond" if $cond;
@@ -75,23 +141,178 @@ sub sel {
   return \@rows;
 }
 
-sub new_key {
-  my ($table, $pk) = @_;
-  $table =~tr/[a-z]/[A-Z]/;
-  my $seq = $table;
-  $seq =~ s/^P_/S_/;
-  my $new_key = sel_one("dual", "$seq.nextval as new_key")->{NEW_KEY};
-  return $new_key;
-}
-
+#------------------------------------------------------------------------------
+# new_row - insert new row
+#------------------------------------------------------------------------------
 sub new_row {
   my ($table, $pk, %rest) = @_;
-  if (not exists $rest{$pk}) {
-    # got no primary key in %rest: generate a new one
-    $rest{$pk} = new_key($table, $pk);
-  }
   ins($table, %rest);
   return sel_one($table, "*", "$pk=$rest{$pk}");
 }
 
 1;
+
+__END__
+# Below is the short of documentation of the module.
+
+=head1 NAME
+
+ODB - a Perl module for accessing database via DBI.
+Means easier handling of the DBI routines via this layer.
+
+=head1 SYNOPSIS
+
+  use ODB;
+
+=head1 DESCRIPTION
+
+=head2 Preface
+
+This module contains functions that are used to connect, disconnect,
+select, insert and delete rows from database.
+Note that BESSY has the copyright on this software. It may not be used
+or copied without permission from BESSY.
+
+=head2 Implemented Functions:
+
+=over 4
+
+=item *
+
+B<interview>
+
+  $dbhandle = ODB::login(%config{
+  			"dbase"=>ORACLE_SID,
+  			"user"=>dbuser,
+  			"passwd"=>dbuserpasswd},
+  			);
+
+This function connect to a Oracle database instance and give back the
+database handle.
+
+=item *
+
+B<interview>
+
+  ODB::logoff();
+
+This function disconnect fairly a Oracle database handle
+
+=item *
+
+B<interview>
+
+  ODB::END();
+
+This function abort the database connection. No rollback!
+
+=item *
+
+B<interview>
+
+  ODB::verbose();
+
+Set debugging output on.
+
+=item *
+
+B<interview>
+
+  ODB::silent();
+
+Set debugging output off.
+
+=item *
+
+B<interview>
+
+  $colstr = ODB::col_aliases($@columns,
+  	$@aliases);
+
+Give the list of real selected columnnames and the aliases as
+refernces of lists. Every column name will be expanded with
+quoted aliases at the same list position.
+
+=item *
+
+B<interview>
+
+  ODB::ins($table,
+  	%row{
+  		columnname=>columnvalue,
+  	...}
+  	);
+
+Insert a new row into the given table on the database handle
+is connected. The hash contains the named list as name to
+value association.
+
+=item *
+
+B<interview>
+
+  ODB::del($table,
+  	$keycolumn,
+  	@keys
+  );
+
+Delete one or more rows from table of the connected handle
+where the keycolumn values in the keys list as select secures
+the content more.
+
+=item *
+
+B<interview>
+
+  ODB::selone($result);
+
+Returns the single dataset of a resultset.
+
+=item *
+
+B<interview>
+
+  %resultset = ODB::sel($table,
+  	$col_names,
+  	$cond,
+  	@bind_values
+  );
+
+Executing the generated select statement on the given handle by
+using the $table as the object clause, columnnames for detecting
+the returned tablecolumns, the filtering conditions and the binded
+variables. In addition is a little buggy feature inside. The resultset
+is returned do NOT return alised column table prefixes such as
+"tbl_test.firstsign". Only "firstsign" will be set as hashkey! Use for
+aliasing the function col_aliases.
+The resultset is returned shows the content like that:
+
+resultset = (
+	{ row1col1=>row1colval1, row1col2=>row1colval2 ...},
+	{ row2col1=>row2colval1, row1col2=>row2colval2 ...},
+	... )
+
+=item *
+
+B<interview>
+
+  %resultset = ODB::new_row($table,
+  	$pk,
+  	%res,
+  );
+
+If you insert a new row you have to give the tablename the
+primary key column and the content of the new row.
+This function trusts to have a simple trigger for the given
+table generated. For example:
+
+  create trigger tbl_name_key_before_insert
+  on tbl_name before insert
+  for each row
+  begin
+    if :new.keycolumn is null or :new.keycolumn <= 0
+    then
+      select tbl_name_seq.nextval into :new.keycolumn from dual;
+    end if;
+  end;
+  /
