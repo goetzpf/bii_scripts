3a7f5c39383344205a20022c415b92418cafbc20
laux <unknown>
2007-02-21 12:37:05 +0000

    Reimplementation. New dbstructure implementation...
    Reimplementation. New dbstructure implementation...
    Problem by setting dblogin !!
    Dont use...

--- bin/bdns_import.pl
+++ bin/bdns_import.pl
@@ -3,7 +3,7 @@ eval 'exec perl -S $0 ${1+"$@"}' # -*- Mode: perl -*-
 
 use FindBin;
 
-# enable this if you want to search modules like dbitable.pm 
+# enable this if you want to search modules like dbitable.pm
 # relative to the location of THIS script:
 # ------------------------------------------------------------
 # use lib "$FindBin::RealBin/../lib/perl";
@@ -13,12 +13,14 @@ use DBI;
 use BDNS;
 use Options;
 use ODB;
+use Data::Dumper;
 
 Options::register(
-  ["dbase",  "d", "=s", "Database instance (e.g. bii_par)", "database", $ENV{'ORACLE_SID'}],
-  ["user",   "u", "=s", "User name",                        "user",     $ENV{'USER'}],
-  ["passwd", "p", "=s", "Password",                         "password", "", 1],
-  ["file",   "f", "=s", "Read (additional) names from a file"]
+  ["dbase",  			"d", "=s", 	"Database instance (e.g. bii_par)", "database", $ENV{'ORACLE_SID'}],
+  ["user",   			"u", "=s", 	"User name", "user",     $ENV{'USER'}],
+  ["passwd", 		"p", "=s", 	"Password", "password", "", 1],
+  ["file",   				"f", "=s", 	"Read (additional) names from a file"],
+  ["dump",   			"d", "", 		"Dont do database actions"],
 );
 
 my $usage = "import a list of names into the bessy device name service
@@ -26,94 +28,59 @@ usage: bdns_import [options] names...
 options:
 ";
 
-my $config = Options::parse($usage);
+my $config = Options::parse($usage, 1);
 
 $usage = $usage . $Options::help;
 
-die $usage if not $config or $config->{"help"};
-
 my @names = @ARGV;
 
-my $handle = ODB::login($config);
-
-Options::print_out("connected as ".$config->{"user"}."@".$config->{"dbase"}."\n");
-
-my $subdomain_keys = get_subdomains($handle);
-my $family_keys = get_families($handle);
-
 if (exists $config->{"file"}) {
-  open INPUT, $config->{"file"} or die "Cannot open file $config->{'file'}";
-  push @names, <INPUT>;
-  close INPUT
+	open INPUT, $config->{"file"} or die "Cannot open file $config->{'file'}";
+	push @names, <INPUT>;
+	close INPUT
 }
 
-die $usage if not @names;
+die $usage if not $config or $config->{"help"} or not @names;
+
+my %allnames;
+warn;
+foreach my $devname (@names) {
+	my @parts = BDNS::parse($devname);
+	die "Error: $devname is not a valid device name!\n" if (not defined @parts);
+	if (! defined @parts[2] or @parts[2] eq "" ) {
+		@parts[2]  = "NULL";
+		@parts[3]  = "NULL";
+	}
+	if (! defined $parts[3] or $parts[3] eq "" ) {
+		@parts[3]  = "NULL";
+	}
+	if (! defined $parts[5] or $parts[5] eq "" ) {
+		@parts[5]  = "NULL";
+	}
+	$allnames{$devname} = {
+		"PART_NAME" => @parts[0],
+		"PART_INDEX"  => @parts[2],
+		"PART_SUBINDEX" => @parts[3],
+		"FAMILY_KEY" => "device.pkg_bdns.get_family_key('".@parts[4]."')",
+		"PART_COUNTER" => @parts[5],
+		"SUBDOMAIN_KEY" => "device.pkg_bdns.get_subdomain_key('".@parts[6]."')",
+	};
+}
+
+warn Dumper(%allnames);
+my $config = Options::ask_out();
+
+my $handle = ODB::login($config);
+Options::print_out("Connected as ".$config->{"user"}."@".$config->{"dbase"}."\n");
 
 foreach my $devname (@names) {
-  my @parts = BDNS::parse($devname);
-
-  if (not defined @parts) {
-    warn "Warning: $devname is not a valid device name!\n";
-    next;
-  };
-
-  my ($member, $allindex, $index, $subindex, $family, $counter, $allsubdomain,
-    $subdomain, $subdompre, $subdompost, $domain, $facility) = @parts;
-
-  $allindex = "'$allindex'";
-  $member = "'$member'";
-
-  $counter = "NULL" if "$counter" eq "";
-  $allindex = "NULL" if "$allindex" eq "''";
-
-  my $sql = "insert into p_name "
-    . "(NAME_KEY, PART_NAME, PART_INDEX, FAMILY_KEY, PART_COUNTER, SUBDOMAIN_KEY) "
-    . "values "
-    . "(s_name.nextval, $member, $allindex, $family_keys->{$family},"
-    . " $counter, $subdomain_keys->{$allsubdomain})";
-
-  print "$sql\n" if $config->{"verbose"};
-  $handle->do($sql);
-  $handle->rollback if $config->{"not"};
+	my $sql = "insert into p_name "
+		. "(PART_NAME, PART_INDEX, PART_SUBINDEX, FAMILY_KEY, PART_COUNTER, SUBDOMAIN_KEY) "
+		. "values "
+		. "(\'".$allnames{"PART_NAME"}."\', \'".$allnames{"PART_INDEX"}."\', \'".$allnames{"PART_SUBINDEX"}."\', ".$allnames{"FAMILY_KEY"}.", \'".$allnames{"PART_COUNTER"}."\', ".$allnames{"SUBDOMAIN_KEY"};
+	print "$sql\n" if $config->{"verbose"};
+	$handle->do($sql) if (! $config->{"dump"});
+	$handle->rollback if $config->{"not"};
 }
+
 exit;
-
-sub get_subdomains {
-  my $result = ODB::sel("SUBDOMAINS", "KEY, NAME||POSTFIX||DOMAIN as VALUE");
-#   my $handle = shift;
-#   my $result = $handle->selectall_hashref("select key, name||postfix||domain as value from subdomains");
-  my $keys;
-  foreach my $row (@$result) {
-    $keys->{$row->{VALUE}} = $row->{KEY};
-  }
-  if ($config->{"verbose"}) {
-    print "subdomains: " . join(",", map("$_=\"$keys->{$_}\"", keys %$keys)) . "\n";
-  }
-  return $keys;
-}
-
-sub get_families {
-  my $dbh = shift;
-  
-  
-  
-# in newer versions of DBI selectall_hashref has different semantics
-# so we emulate it here
-#  my $result = $dbh->selectall_hashref("select family_key as key, part_family as value from p_family");
-  
-  my @rows;
-  my $sth = $dbh->prepare("select family_key as key, part_family as value from p_family");
-  $sth->execute();
-
-  while (my $hash_ref = $sth->fetchrow_hashref) {
-    push @rows, $hash_ref;
-  }
-  
-  
-  my $keys;
-  foreach my $row (@rows) {
-    $keys->{$row->{VALUE}} = $row->{KEY};
-  }
-  #print "families: " . join(",", map("$_=\"$family_keys->{$_}\"", keys %$family_keys)) . "\n";
-  return $keys;
-}
