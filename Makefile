dfdc042b9407505560892976623e9b2024ce8087
Goetz.Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2009-11-19 11:18:53 +0000

    ssh-pw.py was added.
    This script can execute a command on a foreign host via
    ssh. The password (if needed) can be specified as a parameter or by the
    .netrc file.

--- Makefile
+++ Makefile
@@ -306,7 +306,7 @@ PLAINTXT_H_PL_SCRIPT_LIST= \
 # scripts with no embedded documentation
 # create online help by executing "(script.pl -h 2>&1; true)
 PLAINTXT_H_PY_SCRIPT_LIST= \
-	pyone.py sqlutil.py
+	pyone.py sqlutil.py ssh-pw.py
 
 RST_DOC_PY_SCRIPT_LIST= \
 	archiver2camonitor.py \
---
+++ bin/ssh-pw.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+import os
+import sys
+import netrc
+import pexpect
+
+my_version= "1.0"
+
+def parse_mail_addr(st):
+    """parse a mail address name@host."""
+    i= st.find("@")
+    if i<0:
+        return None
+    return (st[0:i],st[i+1:])
+
+def process(options,args):
+    """do the ssh command."""
+    def chk_conflict(key,*args):
+        """check if a key is defined multiple times."""
+        defined= False
+        for a in args:
+            if a.get(key) is not None:
+                if not defined:
+                    defined= True
+                else:
+                    sys.exit("error, multiple definition of \"%s\"" % key)
+    def fill(mandatory,key,main,*args):
+        """fill a dict with contents from other dicts."""
+        if main[key] is not None:
+            return
+        for a in args:
+            v= a.get(key)
+            if v is None:
+                continue
+            main[key]= v
+            return
+        if main[key] is None:
+            if mandatory:
+                sys.exit("error, \"%s\" not specified" % key)
+
+    par_netrc= {}
+    par_options= {}
+    par_args= {}
+
+    if options.netrc:
+        n= netrc.netrc()
+        tp= n.hosts.get(options.netrc)
+        if tp is None:
+            sys.exit("machine name \"%s\" not found in .netrc file" % options.netrc)
+        par_netrc={"host":options.netrc,"user":tp[0],"password":tp[2]}
+
+    if len(args)>0:
+        tp= parse_mail_addr(args[0])
+        if tp is not None:
+            par_args= {"user":tp[0],"host":tp[1]}
+            args= args[1::]
+
+    par_options["user"]    = options.user
+    par_options["password"]= options.password
+    par_options["host"]    = options.host
+
+    # check conflicts:
+    chk_conflict("user"    ,par_options,par_args)
+    chk_conflict("host"    ,par_options,par_args)
+    chk_conflict("password",par_options,par_netrc)
+
+    # fill up missing parts:
+    fill(True ,"user",par_options,par_args,par_netrc)
+    fill(True ,"host",par_options,par_args,par_netrc)
+    fill(False,"password",par_options,par_args,par_netrc)
+
+    argstr= "%s@%s %s" % (par_options["user"],
+                          par_options["host"],
+                          " ".join(args))
+    if options.ssh_A:
+        argstr= "-A "+argstr
+    if options.ssh_args:
+        argstr= options.ssh_args+" "+argstr
+    if options.dry_run:
+        print "ssh %s" % argstr
+        sys.exit(0)
+    child= pexpect.spawn("ssh %s" % argstr, timeout=5)
+    waiting= True
+    while waiting:
+        try:
+            i= child.expect(['[Pp]assword:','yes/no\)\?'])
+            if i==0:
+                child.sendline(par_options["password"])
+                waiting= False
+            elif i==1:
+                child.sendline('yes')
+        except pexpect.EOF:
+            # no password request, just print the 
+            # output so far and exit:
+            print child.before
+            sys.exit(0)
+    child.expect(pexpect.EOF)
+    print child.before
+    sys.exit(0)
+
+def script_shortname():
+    """return the name of this script without a path component."""
+    return os.path.basename(sys.argv[0])
+          
+def print_summary():
+    """print a short summary of the scripts function."""
+    print "%-20s: ssh with password as a commandline option\n" % script_shortname()
+
+def main():
+    """The main function.
+    
+    parse the command-line options and perform the command
+    """
+    # command-line options and command-line help:
+    usage = "usage: %prog [options] {host|user@host} {commands}"
+    parser = OptionParser(usage=usage,
+                          version="%%prog %s" % my_version,
+                          description="execute a command on a foreign host "+\
+                                      "via ssh. The password (if needed) can "+\
+                                      "be specified as a parameter or by the "+\
+                                      ".netrc file.")
+    parser.add_option("--summary",  # implies dest="nodelete"
+                      action="store_true", # default: None
+                      help="print a summary of the function of the program", 
+                      )
+
+    parser.add_option("--dry-run",     # implies dest="switch"
+                      action="store_true", # default: None
+                      help="just show the ssh command",
+                      )
+    parser.add_option("--netrc",     # implies dest="switch"
+                      action="store", # default: None
+                      type="string",  # OptionParser's default
+                      help="read user name and password from .netrc file "+\
+                           "at the given MACHINE entry. If the host is not "+\
+                           "specified otherwise, MACHINE is taken as hostname.",
+                      metavar="MACHINE",
+                      )
+    parser.add_option("-A","--ssh-A",     # implies dest="switch"
+                      action="store_true", # default: None
+                      help="just call ssh with \"-A\"",
+                      )
+
+    parser.add_option("-u","-l", "--user", # implies dest="file"
+                      action="store", # OptionParser's default
+                      type="string",  # OptionParser's default
+                      help="specify the USER", 
+                      metavar="USER"  # for help-generation text
+                      )
+    parser.add_option("-P", "--password", # implies dest="file"
+                      action="store", # OptionParser's default
+                      type="string",  # OptionParser's default
+                      help="specify the PASSWORD", 
+                      metavar="PASSWORD"  # for help-generation text
+                      )
+    parser.add_option("-H", "--host", # implies dest="file"
+                      action="store", # OptionParser's default
+                      type="string",  # OptionParser's default
+                      help="specify the HOST", 
+                      metavar="HOST"  # for help-generation text
+                      )
+    parser.add_option("--ssh-args", # implies dest="file"
+                      action="store", # OptionParser's default
+                      type="string",  # OptionParser's default
+                      help="specify extra SSH-ARGUMENTS for the ssh-call", 
+                      metavar="SSH-ARGUMENTS"  # for help-generation text
+                      )
+
+    (options, args) = parser.parse_args()
+    # options: the options-object
+    # args: list of left-over args
+
+    if options.summary:
+        print_summary()
+        sys.exit(0)
+        
+    process(options,args)
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
+
