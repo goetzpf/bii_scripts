4dac50e4f928b3c6b657627d3d91d4de6c5c0b92
benjamin.franksen <benjamin.franksen@bessy.de>
2010-03-08 14:17:34 +0000

    use more reliable method of obtaining host name

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo "darcs pull is executed in order to prevent"
 echo "you from installing old program versions by accident"
 darcs pull
 make all
-if test $(uname -n) != "aragon"; then
+if test $(hostname -f) != "aragon.acc.bessy.de"; then
   sg scrptdev -c "make install"
 else
   make install
