878ca16b28c57f4df26badd5553f64becd933a96
Bernhard.Kuner <Bernhard.Kuner@bessy.de>
2011-11-16 13:42:01 +0000

    FIX: dont't write empty files (bin/applicationIndex.py)

--- bin/applicationIndex.py
+++ bin/applicationIndex.py
@@ -19,6 +19,31 @@ def systemCall(cmdArgsList):
 	print >>sys.stderr, "Execution failed:",cmdArgsList, e
 	return None
     
+def getIoc(dbIoc,db):
+    """ little html helper
+    """
+    if dbIoc.has_key(db):   
+    	return reduce(lambda y,x:y+str("<A HREF=\"#"+x+"\">"+x+"</A><br>"),dbIoc[db],"")
+    else: 
+    	return "not loaded on IOC"
+
+def getDb(dbApp,db):
+    """ little html helper
+    """
+    if dbApp.has_key(db): 
+    	return "<DIV TITLE=\""+dbApp[db]+"\"><A HREF=\"#"+dbApp[db]+"\">"+db+"</A></DIV>"
+    else: 
+    	return "<DIV TITLE=\"got from support module\">"+db+"</DIV>"
+
+def toCol(dList,tag='TD'): 
+    """ create a string of tagged list items, default tag is TD, also possible: TH, LI"""
+    def toStr(x):
+    	if x is None:
+	    return "-"
+	else:
+	    return str(x)
+    return "<"+tag+">"+str("</"+tag+">\n  <"+tag+">").join([toStr(x) for x in dList])+"</"+tag+">"
+
 def processStCmd(topPath):
     iocBootPath = topPath+"/iocBoot"
     iocString = systemCall(['ls',iocBootPath])
@@ -138,7 +163,7 @@ def checkHardwareAccess(iocDb,topPath):
     return hwData
 	    	
 ######## Main #######################################################
-usage        = " CreateOpt.py [options] topPath ioc dbOutPath"
+usage        = "USAGE: applicationIndex.py [options] topPath outFile"
 parser = OptionParser(usage=usage,
 		 version="%%prog 1.0",
     		 description="USAGE: applictionIndex.py [-h OPTIONS] topPath")
@@ -148,31 +173,23 @@ parser.add_option("-v","--verbose",
     		 )
 try:
     (options,args) = parser.parse_args()
-    (topPath,) = args
+    (topPath,filename) = args[0:2]
 except:
+    print "OPTIONS:",options,"\nARGS:",args[:2]
     print usage
     sys.exit()
 
 (appDb,dbApp) = findApplications(topPath)
-#pp.pprint(appDb)
-#pp.pprint(dbApp)
 (iocDb,dbIoc) = processStCmd(topPath)
-#pp.pprint(iocDb)
-#pp.pprint(dbIoc)
-#order = ('port','nid','cid','CARD','CHAN','LINK','pvname','filename')
-#hwList = checkHardwareAccess({'IOC':['test.db']},topPath)
-#(canList,otherList) = lod.filterMatch(hwList,{'DTYP':['lowcal'],'PORT':[1],'nid':[1]})
-#table = lod.orderToTable(canList,order)
-#eU.printTable(table,order)
-#pp.pprint(otherList)
-#sys.exit()
-iocHw = checkHardwareAccess(iocDb,topPath)
-#pp.pprint(iocHw)
-#sys.exit()
+iocHw = None
+if iocDb:
+    iocHw = checkHardwareAccess(iocDb,topPath)
+
 ######## PRINT DATA #######################################################
 htmlHeader = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
 <head>
+    <TITLE>Application and Hardware Reference</TITLE>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link rel=stylesheet type="text/css" href="http://www-csr.bessy.de/~kuner/makeDocs/docStyle.css">
 </head>
@@ -183,7 +200,9 @@ htmlFooter = """
 </body>
 </html>
 """
-filename = "reference.html"
+if not appDb and not iocDb and not iocHw: # nothing to do
+    sys.exit()
+
 try :
     FILE = open(filename,"w") 
 except IOError: 
@@ -194,93 +213,74 @@ print >> FILE, htmlHeader
 # Application Reference:
 if options.verbose is True: print "********** Write File *********"
 print >> FILE, "<H1>Application Reference</H1>\n\n<TABLE BORDER=1>"
-def getIoc(dbIoc,db):
-    if dbIoc.has_key(db):   
-    	return reduce(lambda y,x:y+str("<A HREF=\"#"+x+"\">"+x+"</A><br>"),dbIoc[db],"")
-    else: 
-    	return "not loaded on IOC"
 
-for app in appDb.keys():
-    dbList = appDb[app]
-    span = ""
-    if len(dbList) > 1:
-    	span = "ROWSPAN=\""+str(len(dbList))+"\" "
-    print >> FILE, "<TR >\n  <TD "+span+"VALIGN=\"TOP\"><A NAME=\""+app+"\">"+app+"</TD>\n  <TD VALIGN=\"TOP\">"+dbList[0]+"</TD>\n  <TD>"+getIoc(dbIoc,dbList[0])+"</TD></TR>"
-    for db in dbList[1:]:
-	print >> FILE,"<TR>\n  <TD VALIGN=\"TOP\">"+db+"</TD>\n  <TD VALIGN=\"TOP\">"+getIoc(dbIoc,db)+"</TD></TR>"
-print >> FILE, "</TABLE>\n"
+if appDb:
+    for app in appDb.keys():
+	dbList = appDb[app]
+	span = ""
+	if len(dbList) > 1:
+    	    span = "ROWSPAN=\""+str(len(dbList))+"\" "
+	print >> FILE, "<TR >\n  <TD "+span+"VALIGN=\"TOP\"><A NAME=\""+app+"\">"+app+"</TD>\n  <TD VALIGN=\"TOP\">"+dbList[0]+"</TD>\n  <TD>"+getIoc(dbIoc,dbList[0])+"</TD></TR>"
+	for db in dbList[1:]:
+	    print >> FILE,"<TR>\n  <TD VALIGN=\"TOP\">"+db+"</TD>\n  <TD VALIGN=\"TOP\">"+getIoc(dbIoc,db)+"</TD></TR>"
+    print >> FILE, "</TABLE>\n"
 
-print >> FILE, '<H1>IOC Application Reference</H1>\n\n<TABLE BORDER=1>'
+if iocDb:
+    print >> FILE, '<H1>IOC Application Reference</H1>\n\n<TABLE BORDER=1>'
+    for ioc in iocDb.keys():
+	dbList = iocDb[ioc]
+	span = ""
+	if len(dbList) > 1:
+    	    span = "ROWSPAN=\""+str(len(dbList))+"\" "
+	setIoc = '<TH '+span+'VALIGN="TOP"><A NAME="'+ioc+'"><A HREF="#HW_'+ioc+'">'+ioc+'</A></TH>\n  '
+	for db in dbList:
+	    print >> FILE,'<TR>\n  '+setIoc+'<TD>'+getDb(dbApp,db)+'</TD></TR>'
+	    if len(setIoc) > 0: 	# first item only
+		setIoc = ''
+    print >> FILE, "</TABLE>\n"
 
-def getDb(dbApp,db):
-    if dbApp.has_key(db): 
-    	return "<DIV TITLE=\""+dbApp[db]+"\"><A HREF=\"#"+dbApp[db]+"\">"+db+"</A></DIV>"
-    else: 
-    	return "<DIV TITLE=\"got from support module\">"+db+"</DIV>"
+if iocHw:
+    print >> FILE, "<H1>IOC Hardware Reference</H1>\n\n"
+    (forgetThisList,getHw) = lod.filterMatch(iocHw,{'DTYP':['HwClient','Dist Version','IOC stats','Raw Soft Channel','Soft Channel','Soft Timestamp','Soft Timestamp WA','VX stats','VxWorks Boot Parameter']})
+    for ioc in iocDb.keys():
+	(iocHwList,getHw) = lod.filterMatch(getHw,{'iocname':ioc})
+	if len(iocHwList) == 0: 
+    	    continue
 
-for ioc in iocDb.keys():
-    dbList = iocDb[ioc]
-    span = ""
-    if len(dbList) > 1:
-    	span = "ROWSPAN=\""+str(len(dbList))+"\" "
-    setIoc = '<TH '+span+'VALIGN="TOP"><A NAME="'+ioc+'"><A HREF="#HW_'+ioc+'">'+ioc+'</A></TH>\n  '
-    for db in dbList:
-	print >> FILE,'<TR>\n  '+setIoc+'<TD>'+getDb(dbApp,db)+'</TD></TR>'
-	if len(setIoc) > 0: 	# first item only
-	    setIoc = ''
-print >> FILE, "</TABLE>\n"
+	print >> FILE, '<H2><A NAME="HW_'+ioc+'">'+ioc+'</H2>\n\n'
 
-print >> FILE, "<H1>IOC Hardware Reference</H1>\n\n"
+	print >> FILE, "<H3>CAN Devices</H3>\n\n"
+	order = ('port','nid','cid','CARD','CHAN','LINK','pvname','filename')
+	(canList,otherList) = lod.filterMatch(iocHwList,{'DTYP':['lowcal',],})
+	table = lod.orderToTable(canList,order)
+	if len(table) > 0:
+            print >> FILE, '<TABLE BORDER=1>\n<TR>'+toCol(['Process Variable','Port','CAN-Id','Card','Chan','Link','cid'],'TH')+'\n</TR>'
+            for (port,nid,cid,CARD,CHAN,LINK,pvname,filename) in table:
+        	c = LINK.split(' ') # c=(@type dataType port in_cob out_cob mux ....)
+		t = "In Cob: %d Out Cob: %d Mux: %d" % (int(c[4],16),int(c[5],16),int(c[6],16))
+        	LINK = '<DIV TITLE="'+t+'">'+LINK+'</DIV>'
+        	pvname = '<DIV TITLE="IOC: '+ioc+', Application: '+dbApp[filename]+', File: '+filename+'">'+pvname+'</DIV>'
+        	print >> FILE, "<TR>"+toCol([pvname,port,nid,CARD,CHAN,LINK,cid])+"\n</TR>"
+            print >> FILE, "</TABLE>\n"
 
-def toCol(dList,tag='TD'): 
-    """ create a string of tagged list items, default tag is TD, also possible: TH, LI"""
-    def toStr(x):
-    	if x is None:
-	    return "-"
-	else:
-	    return str(x)
-    return "<"+tag+">"+str("</"+tag+">\n  <"+tag+">").join([toStr(x) for x in dList])+"</"+tag+">"
+	print >> FILE, "<H3>VME Devices</H3>\n\n"
+	(vmeList,otherList) = lod.filterMatch(otherList,{'DTYP':['esd AIO16','ADA16','BESSY MuxV','Dyncon','EK IO32','ESRF MuxV','Highland DDG V85x','OMS MAXv','OMS VME58','Rfmux1366','TDU','V375','V680','VHQ','Vpdu']})
+	order = ('DTYP','CARD','CHAN','pvname','LINK')
+	table = lod.orderToTable(vmeList,order)
+	if len(table) > 0:
+            print >> FILE, "<TABLE BORDER=1>\n<TR>"+toCol(['Process Variable','Card','Chan','DTYP','Link'],'TH')+"\n</TR>"
+            for (DTYP,CARD,CHAN,pvname,LINK) in table:
+        	print >> FILE, "<TR>"+toCol([pvname,CARD,CHAN,DTYP,LINK])+"\n</TR>"
+            print >> FILE, "</TABLE>\n"
 
-(forgetThisList,getHw) = lod.filterMatch(iocHw,{'DTYP':['HwClient','Dist Version','IOC stats','Raw Soft Channel','Soft Channel','Soft Timestamp','Soft Timestamp WA','VX stats','VxWorks Boot Parameter']})
-for ioc in iocDb.keys():
-    (iocHwList,getHw) = lod.filterMatch(getHw,{'iocname':ioc})
-    if len(iocHwList) == 0: 
-    	continue
-
-    print >> FILE, '<H2><A NAME="HW_'+ioc+'">'+ioc+'</H2>\n\n'
-
-    print >> FILE, "<H3>CAN Devices</H3>\n\n"
-    order = ('port','nid','cid','CARD','CHAN','LINK','pvname','filename')
-    (canList,otherList) = lod.filterMatch(iocHwList,{'DTYP':['lowcal',],})
-    table = lod.orderToTable(canList,order)
-    if len(table) > 0:
-        print >> FILE, '<TABLE BORDER=1>\n<TR>'+toCol(['Process Variable','Port','CAN-Id','Card','Chan','Link','cid'],'TH')+'\n</TR>'
-        for (port,nid,cid,CARD,CHAN,LINK,pvname,filename) in table:
-            c = LINK.split(' ') # c=(@type dataType port in_cob out_cob mux ....)
-	    t = "In Cob: %d Out Cob: %d Mux: %d" % (int(c[4],16),int(c[5],16),int(c[6],16))
-            LINK = '<DIV TITLE="'+t+'">'+LINK+'</DIV>'
-            pvname = '<DIV TITLE="IOC: '+ioc+', Application: '+dbApp[filename]+', File: '+filename+'">'+pvname+'</DIV>'
-            print >> FILE, "<TR>"+toCol([pvname,port,nid,CARD,CHAN,LINK,cid])+"\n</TR>"
-        print >> FILE, "</TABLE>\n"
-
-    print >> FILE, "<H3>VME Devices</H3>\n\n"
-    (vmeList,otherList) = lod.filterMatch(otherList,{'DTYP':['esd AIO16','ADA16','BESSY MuxV','Dyncon','EK IO32','ESRF MuxV','Highland DDG V85x','OMS MAXv','OMS VME58','Rfmux1366','TDU','V375','V680','VHQ','Vpdu']})
-    order = ('DTYP','CARD','CHAN','pvname','LINK')
-    table = lod.orderToTable(vmeList,order)
-    if len(table) > 0:
-        print >> FILE, "<TABLE BORDER=1>\n<TR>"+toCol(['Process Variable','Card','Chan','DTYP','Link'],'TH')+"\n</TR>"
-        for (DTYP,CARD,CHAN,pvname,LINK) in table:
-            print >> FILE, "<TR>"+toCol([pvname,CARD,CHAN,DTYP,LINK])+"\n</TR>"
-        print >> FILE, "</TABLE>\n"
-
-    print >> FILE, "<H3>Other Devices</H3>\n\n"
-    order = ('LINK','pvname')
-    table = lod.orderToTable(otherList,order)
-    if len(table) > 0:
-        print >> FILE, "<TABLE BORDER=1>\n<TR>"+toCol(['Process Variable','Link'],'TH')+"\n</TR>"
-        for (link,pv) in table:
-            print >> FILE, "<TR>"+toCol([pv,link])+"\n</TR>"
-        print >> FILE, "</TABLE>\n"
+	print >> FILE, "<H3>Other Devices</H3>\n\n"
+	order = ('LINK','pvname')
+	table = lod.orderToTable(otherList,order)
+	if len(table) > 0:
+            print >> FILE, "<TABLE BORDER=1>\n<TR>"+toCol(['Process Variable','Link'],'TH')+"\n</TR>"
+            for (link,pv) in table:
+        	print >> FILE, "<TR>"+toCol([pv,link])+"\n</TR>"
+            print >> FILE, "</TABLE>\n"
 
 print >> FILE, htmlFooter
 FILE.close()
