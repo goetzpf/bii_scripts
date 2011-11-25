3bf26177940f9ef40506f7b00d49721f0482c373
Bernhard.Kuner <Bernhard.Kuner@bessy.de>
2011-11-25 07:14:40 +0000

    FIX: first realy working version (./bin/applicationIndex.py)

--- bin/applicationIndex.py
+++ bin/applicationIndex.py
@@ -2,6 +2,7 @@
 
 from optparse import OptionParser
 import sys
+import re
 import os
 import os.path
 import subprocess
@@ -9,6 +10,14 @@ import epicsUtils as eU
 import listOfDict as lod
 import pprint as pp
 import canLink as cL
+
+# Searching for Hardware means to look at the DTYP field of a record. So here we define
+# the list of known DTYPs get it with:
+# find $(TOP)/db -name *.db|xargs perl -e 'while(<>){print "$1\n" if($_=~/DTYP,\s*\"(.*)\"/);}'|sort -u
+hardwareDtypList = ['esd AIO16','ADA16','BESSY MuxV','Dyncon','EK IO32','ESRF MuxV',
+    	    	    'Highland DDG V85x','OMS MAXv','OMS VME58','Rfmux1366','TDU','V375',
+		    'V680','VHQ','Vpdu'
+		   ]
 
 def systemCall(cmdArgsList):
     """ Do a system call and return the output
@@ -44,12 +53,18 @@ def toCol(dList,tag='TD'):
 	    return str(x)
     return "<"+tag+">"+str("</"+tag+">\n  <"+tag+">").join([toStr(x) for x in dList])+"</"+tag+">"
 
+def substEnvVariables(param,envDict):
+    for name in envDict.keys():
+	param = param.replace(r"${"+name+"}",envDict[name])
+    return param
+
 def processStCmd(topPath):
     iocBootPath = topPath+"/iocBoot"
     iocString = systemCall(['ls',iocBootPath])
     if options.verbose is True: print "processStCmd: ",topPath
     iocDb = {}
     dbIoc = {}
+
     for ioc in iocString.split("\n"):
 	i = eU.matchRe(ioc,"ioc(.*)")
 	if i is None:
@@ -67,27 +82,28 @@ def processStCmd(topPath):
 	dbdFile=topPath+"/dbd/"
  	envDict={}
 
-	iocDb[iocName] = []
 	for line in IN_FILE:
 	    parsedLine = eU.parseStCmd(line)
+
 	    if len(parsedLine)<1: continue
 	    cmd = parsedLine[0]
-	#    print "PARSE: '%s'" %line, parsedLine
+	    #print "PARSE: '%s'" %line, parsedLine
 	    if cmd == "epicsEnvSet":
-	#    	print "PARSE: '%s'" %line, parsedLine
+       		#print "PARSE: '%s'" %line, parsedLine
     		envDict[parsedLine[1]]=parsedLine[2]
 	    if cmd == "putenv":
 		(name,value)=parsedLine[1].split("=")
 		envDict[name]=value
 	    if cmd == "dbLoadDatabase":
-		dbdFile += eU.substituteVariables(eU.substRe(parsedLine[1],"dbd/",""),envDict)
+    	    	dbdFile += substEnvVariables(eU.substRe(parsedLine[1],"dbd/",""),envDict)
 	    if cmd == "dbLoadRecords":
-    		dbFile = eU.substituteVariables(eU.substRe(parsedLine[1],"db/",""),envDict)
-    		if options.verbose is True: print "\t",dbFile
-		iocDb[iocName].append(dbFile)
-    	    	if not dbIoc.has_key(dbFile):
-		    dbIoc[dbFile] = []
-		dbIoc[dbFile].append(iocName)
+    		dbFile = substEnvVariables(eU.substRe(parsedLine[1],"db/",""),envDict)
+		param = ""
+		if len(parsedLine) == 3:
+		    param = parsedLine[2]
+		if not iocDb.has_key(iocName):
+	    	    iocDb[iocName] = []
+		iocDb[iocName].append((iocName,dbFile,eU.parseParam(substEnvVariables(param,envDict),',')))
     return (iocDb,dbIoc)
 
 def findApplications(topPath):
@@ -111,21 +127,29 @@ def findApplications(topPath):
 	    dbApp[dbFileName] = appName
     return (appDb,dbApp)
 
-def hardware(filename,iocname,pvname,fieldDict) :
-    fieldDict.update( {"iocname":iocname,"filename":filename,"pvname":pvname} )
+def hardware(ioc,dbFile,param,iocname,pvname,fieldDict) :
+    
+    pvname = eU.substituteVariables(pvname,param)
+    fieldDict.update( {"iocname":iocname,"filename":dbFile,"pvname":pvname} )
 
     if fieldDict.has_key('INP'):
-        fieldDict['LINK'] = fieldDict['INP']
+        link = fieldDict['INP']
     elif fieldDict.has_key('OUT'):
-        fieldDict['LINK'] = fieldDict['OUT']
+        link = fieldDict['OUT']
     else:
-        fieldDict['LINK'] = None
-
+        link = None
+    if link and len(param) >0:
+	link = eU.substituteVariables(link,param)
     if fieldDict['DTYP'] == 'lowcal':
-        if fieldDict['RTYP'] == 'hwLowcal':
-            fieldDict['LINK'] = cL.hwLowcal2canLink(fieldDict)
-        fieldDict.update(cL.decode(fieldDict['LINK']))
-        if fieldDict.has_key('cid') and fieldDict['cid'] == 2:	# ADA/IO32 card
+        try:
+	    if fieldDict['RTYP'] == 'hwLowcal':
+        	link = cL.hwLowcal2canLink(fieldDict)
+	    if link:
+		fieldDict.update(cL.decode(link))
+        except ValueError, e:
+	    print "Warning:",dbFile+", '"+fieldDict['pvname']+"': ",e
+	    
+	if fieldDict.has_key('cid') and fieldDict['cid'] == 2:	# ADA/IO32 card
             mux = int(fieldDict['multiplexor'])
             fieldDict['CARD'] =  mux/12
             fieldDict['CHAN'] =  mux%12
@@ -134,17 +158,22 @@ def hardware(filename,iocname,pvname,fieldDict) :
             fieldDict['CARD'] =  mux/2
             fieldDict['CHAN'] =  mux%2
     else:
-        vmeLnk = eU.matchRe(fieldDict['LINK'],"#C\s*(\d+)\s*S\s*(\d+)")
+        vmeLnk = eU.matchRe(link,"#C\s*(\d+)\s*S\s*(\d+)")
         if vmeLnk is not None:
             fieldDict['CARD'] =  vmeLnk[0]
             fieldDict['CHAN'] =  vmeLnk[1]
+	else:
+	    if link.find ("$")>=0:
+	    	print "hardware() $",iocname,"filename",bFile,"pvname",pvname,"link",link
+		
+    fieldDict['LINK'] = link    	    
     return fieldDict
 
 def checkHardwareAccess(iocDb,topPath):
     hwData = []
     for ioc in iocDb.keys():
-        for db in iocDb[ioc]:
-            hw = systemCall(['grepDb.pl','-pH','-th',topPath+"/db/"+db]) # return a perl hash of {PVNAME=> {FIELD=>VALUE}}
+        for (ioc,dbFile,param) in iocDb[ioc]:
+            hw = systemCall(['grepDb.pl','-pH','-th',topPath+"/db/"+dbFile]) # return a perl hash of {PVNAME=> {FIELD=>VALUE}}
             if not hw:
                 continue 
             hw = eU.substRe(hw," => ",":")  	# make it python eval uable
@@ -156,9 +185,9 @@ def checkHardwareAccess(iocDb,topPath):
                 pp.pprint(hw)
                 print "ERROR in checkHardwareAccess(",iocDb,topPath,")"
                 sys.exit()
-            for pv in hwDict.keys():
-                hw = hardware(db,ioc,pv,hwDict[pv])
-                if hw: 
+	    for pv in hwDict.keys():
+		hw = hardware(ioc,dbFile,param,ioc,pv,hwDict[pv])
+		if hw: 
                     hwData.append(hw)
     return hwData
 	    	
@@ -181,6 +210,8 @@ except:
 
 (appDb,dbApp) = findApplications(topPath)
 (iocDb,dbIoc) = processStCmd(topPath)
+#pp.pprint(iocDb)
+#sys.exit()
 iocHw = None
 if iocDb:
     iocHw = checkHardwareAccess(iocDb,topPath)
@@ -227,14 +258,14 @@ if appDb:
 
 if iocDb:
     print >> FILE, '<H1>IOC Application Reference</H1>\n\n<TABLE BORDER=1>'
-    for ioc in iocDb.keys():
+    for ioc in sorted(iocDb.keys()):
 	dbList = iocDb[ioc]
 	span = ""
 	if len(dbList) > 1:
     	    span = "ROWSPAN=\""+str(len(dbList))+"\" "
 	setIoc = '<TH '+span+'VALIGN="TOP"><A NAME="'+ioc+'"><A HREF="#HW_'+ioc+'">'+ioc+'</A></TH>\n  '
-	for db in dbList:
-	    print >> FILE,'<TR>\n  '+setIoc+'<TD>'+getDb(dbApp,db)+'</TD></TR>'
+	for dbObj in dbList:
+	    print >> FILE,'<TR>\n  '+setIoc+'<TD>'+getDb(dbApp,dbObj[1])+'</TD></TR>'
 	    if len(setIoc) > 0: 	# first item only
 		setIoc = ''
     print >> FILE, "</TABLE>\n"
@@ -255,22 +286,34 @@ if iocHw:
 	table = lod.orderToTable(canList,order)
 	if len(table) > 0:
             print >> FILE, '<TABLE BORDER=1>\n<TR>'+toCol(['Process Variable','Port','CAN-Id','Card','Chan','Link','cid'],'TH')+'\n</TR>'
-            for (port,nid,cid,CARD,CHAN,LINK,pvname,filename) in table:
-        	c = LINK.split(' ') # c=(@type dataType port in_cob out_cob mux ....)
-		t = "In Cob: %d Out Cob: %d Mux: %d" % (int(c[4],16),int(c[5],16),int(c[6],16))
-        	LINK = '<DIV TITLE="'+t+'">'+LINK+'</DIV>'
-        	pvname = '<DIV TITLE="IOC: '+ioc+', Application: '+dbApp[filename]+', File: '+filename+'">'+pvname+'</DIV>'
-        	print >> FILE, "<TR>"+toCol([pvname,port,nid,CARD,CHAN,LINK,cid])+"\n</TR>"
-            print >> FILE, "</TABLE>\n"
+            try:
+		for (port,nid,cid,CARD,CHAN,LINK,pvname,filename) in table:
+		    if LINK is not None:
+			c = LINK.split(' ') # c=(@type dataType port in_cob out_cob mux ....)
+			t = "In Cob: %d Out Cob: %d Mux: %d" % (int(c[4],16),int(c[5],16),int(c[6],16))
+		    else:
+			t = "No INP/OUT"
+			LINK = '-'
+        	    LINK = '<DIV TITLE="'+t+'">'+LINK+'</DIV>'
+		    pvname = '<DIV TITLE="IOC: '+ioc+', Application: '+dbApp[filename]+', File: '+filename+'">'+pvname+'</DIV>'
+		    print >> FILE, "<TR>"+toCol([pvname,port,nid,CARD,CHAN,LINK,cid])+"\n</TR>"
+            except:
+	    	print "ERROR in print:",filename,pvname
+	    print >> FILE, "</TABLE>\n"
 
 	print >> FILE, "<H3>VME Devices</H3>\n\n"
-	(vmeList,otherList) = lod.filterMatch(otherList,{'DTYP':['esd AIO16','ADA16','BESSY MuxV','Dyncon','EK IO32','ESRF MuxV','Highland DDG V85x','OMS MAXv','OMS VME58','Rfmux1366','TDU','V375','V680','VHQ','Vpdu']})
+	(vmeList,otherList) = lod.filterMatch(otherList,{'DTYP':hardwareDtypList})
 	order = ('DTYP','CARD','CHAN','pvname','LINK')
 	table = lod.orderToTable(vmeList,order)
 	if len(table) > 0:
             print >> FILE, "<TABLE BORDER=1>\n<TR>"+toCol(['Process Variable','Card','Chan','DTYP','Link'],'TH')+"\n</TR>"
-            for (DTYP,CARD,CHAN,pvname,LINK) in table:
-        	print >> FILE, "<TR>"+toCol([pvname,CARD,CHAN,DTYP,LINK])+"\n</TR>"
+	    try:
+        	for l in table:
+		    (DTYP,CARD,CHAN,pvname,LINK) = l
+    	    	    pvname = '<DIV TITLE="IOC: '+ioc+', Application: '+dbApp[filename]+', File: '+filename+'">'+pvname+'</DIV>'
+        	    print >> FILE, "<TR>"+toCol([pvname,CARD,CHAN,DTYP,LINK])+"\n</TR>"
+            except:
+	    	print "ERROR in print:",filename,pvname
             print >> FILE, "</TABLE>\n"
 
 	print >> FILE, "<H3>Other Devices</H3>\n\n"
@@ -278,8 +321,12 @@ if iocHw:
 	table = lod.orderToTable(otherList,order)
 	if len(table) > 0:
             print >> FILE, "<TABLE BORDER=1>\n<TR>"+toCol(['Process Variable','Link'],'TH')+"\n</TR>"
-            for (link,pv) in table:
-        	print >> FILE, "<TR>"+toCol([pv,link])+"\n</TR>"
+	    try:
+        	for (link,pv) in table:
+        	    pvname = '<DIV TITLE="IOC: '+ioc+', Application: '+dbApp[filename]+', File: '+filename+'">'+pvname+'</DIV>'
+		    print >> FILE, "<TR>"+toCol([pvname,link])+"\n</TR>"
+            except:
+	    	print "ERROR in print:",filename,pvname
             print >> FILE, "</TABLE>\n"
 
 print >> FILE, htmlFooter
