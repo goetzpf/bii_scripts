67dabbe60f80505643f1b71a1ee6000f9f53b211
Goetz.Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2020-06-17 12:50:05 +0000

    Bugfix: recreate bii_scripts.config each time install.sh is run.

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ if [ -z "$1" ]; then
         echo "darcs pull is executed in order to prevent"
         echo "you from installing old program versions by accident"
         darcs pull
+        # force bii_scripts.config to be remade:
+        rm -f out/script/bii_scripts.config
         BII_CONFIG=config.$SHORTHOST.acc make -sj install
         exit
     fi
@@ -30,5 +32,8 @@ fi
 ABS_INSTALLDIR=$(readlink -e "$INSTALLDIR")
 
 #mkdir -p $INSTALLDIR/bin $INSTALLDIR/share/html/bii_scripts $INSTALLDIR/lib/perl $INSTALLDIR/lib/python
+
+# force bii_scripts.config to be remade:
+rm -f out/script/bii_scripts.config
 INSTALL_PREFIX=$INSTALLDIR make -sj install
 
