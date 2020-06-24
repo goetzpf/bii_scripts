66f4de3202a6dbad43b14ab5c6e4173f899767f1
Goetz Pfeiffer <goetzpf@googlemail.com>
2020-06-24 08:57:12 +0000

    The file SETENV-RUN-LOCAL.sh now checks the current working directory first.
    This script modifies the environment variables PATH, PERL5LIB and PYTHONPATH so
    that scripts in the bii_scripts source tree can be run directly. For this it
    must know the locatation of the bii_scripts source tree which it takes from the
    current working directory. If the bii_scripts source directory is not found
    there, the script aborts.
    
    The script must be *sourced* not *executed* like here:
    
      . ./SETENV-RUN-LOCAL.sh

--- SETENV-RUN-LOCAL.sh
+++ SETENV-RUN-LOCAL.sh
@@ -2,13 +2,34 @@
 
 BII_SCRIPTS_TOP=$PWD
 
-# make scripts executable:
-chmod u+rwx $BII_SCRIPTS_TOP/bin/*
+BII_SCRIPTS_BIN="$BII_SCRIPTS_TOP/bin"
+BII_SCRIPTS_LIB_PERL="$BII_SCRIPTS_TOP/lib/perl"
+BII_SCRIPTS_LIB_PYTHON="$BII_SCRIPTS_TOP/lib/python"
 
-# set environment:
-export PERL5LIB=$BII_SCRIPTS_TOP/lib/perl:$PERL5LIB
-export PYTHONPATH=$BII_SCRIPTS_TOP/lib/python:$PYTHONPATH
-PATH=$BII_SCRIPTS_TOP/bin:$PATH
+# check if we are in the right directory:
+dir_ok="yes"
+if [ ! -d "$BII_SCRIPTS_BIN" ]; then
+    dir_ok=""
+fi
+if [ ! -d "$BII_SCRIPTS_LIB_PERL" ]; then
+    dir_ok=""
+fi
+if [ ! -d "$BII_SCRIPTS_LIB_PYTHON" ]; then
+    dir_ok=""
+fi
 
-# set prompt:
-PS1="bii-scr $PS1"
+if [ -z "$dir_ok" ]; then
+    echo "error: Your current working directory must be the"
+    echo "       bii_scripts project, aborting."
+else
+    # make scripts executable:
+    chmod u+rwx $BII_SCRIPTS_BIN/*
+
+    # set environment:
+    export PERL5LIB=$BII_SCRIPTS_LIB_PERL:$PERL5LIB
+    export PYTHONPATH=$BII_SCRIPTS_LIB_PYTHON:$PYTHONPATH
+    PATH=$BII_SCRIPTS_BIN:$PATH
+
+    # set prompt:
+    PS1="bii-scr $PS1"
+fi
