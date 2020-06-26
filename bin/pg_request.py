d7aa9f02353dafff96d04a10d50499d498cf3322
Goetz Pfeiffer <goetzpf@googlemail.com>
2020-06-26 12:39:07 +0000

    pg_request.py: The database cache now uses file locking.

--- bin/pg_request.py
+++ bin/pg_request.py
@@ -51,9 +51,12 @@ Features:
 
 """
 
-#pylint: disable= invalid-name, bad-whitespace
+#pylint: disable= invalid-name, bad-whitespace, too-many-lines
 
 import sys
+import platform
+import errno
+import time
 import os
 import pprint
 import json
@@ -92,6 +95,7 @@ VERSION = "1.0"
 SCRIPTNAME= os.path.basename(__file__)
 
 CONNECT_TIMEOUT= 5 # seconds
+LOCK_TIMEOUT   = 10 # seconds
 
 SQL_SHOW_TABLES="SELECT  table_schema , table_name  " \
                 "FROM information_schema.tables " \
@@ -124,6 +128,138 @@ PROFILES= { \
 
 DEFAULT_PROFILE= "devices2015"
 
+# file lock class, taken from sumo
+
+class LockedError(Exception):
+    """This is raised when we can't get a lock."""
+
+class AccessError(Exception):
+    """No rights to create a lock.
+
+    This is raised when we can't create a lock due to access rights on the
+    directory
+    """
+
+class NoSuchFileError(Exception):
+    """Cannot create lock, path does not exist.
+
+    This is raised when we can't create a lock since the file path where we
+    want to create it doesn't exist.
+    """
+
+class MyLock():
+    """Implement a simple file locking mechanism."""
+    def lock(self):
+        """do the file locking.
+
+        May raise:
+            LockedError     : can't get lock
+            AccessError     : no rights to create lock
+            NoSuchFileError : file path doesn't exist
+            OSError         : other operating system errors
+
+        On linux, create a symbolic link, otherwise a directory. The symbolic
+        link has some information on the user, host and process ID.
+
+        On other systems the created directory contains a file whose name has
+        some information on the user, host and process ID.
+        """
+        # pylint: disable=too-many-branches
+        if self._disabled:
+            return
+        if self._has_lock:
+            raise AssertionError("cannot lock '%s' twice" % self._filename)
+        self.info="%s@%s:%s" % (getpass.getuser(),
+                                platform.node(),
+                                os.getpid())
+        tmo= self.timeout
+        while True:
+            try:
+                if self.method=="link":
+                    os.symlink(self.info, self.lockname)
+                else:
+                    os.mkdir(self.lockname)
+                    open(os.path.join(self.lockname,self.info),'w').close()
+            except OSError as e:
+                # pylint: disable=no-else-raise
+                # probably "File exists"
+                if e.errno==errno.EEXIST:
+                    if tmo>0:
+                        tmo-= 1
+                        time.sleep(1)
+                        continue
+                    if self.method=="link":
+                        raise LockedError("file '%s' is locked: %s" % \
+                                      (self._filename,
+                                       os.readlink(self.lockname)))
+                    else:
+                        txt= " ".join(os.listdir(self.lockname))
+                        raise LockedError("file '%s' is locked: %s" % \
+                                      (self._filename, txt))
+                elif e.errno==errno.EACCES:
+                    # cannot write to directory
+                    raise AccessError(("no rights to create lock for "
+                                       "file '%s'") % self._filename)
+                elif e.errno==errno.ENOENT:
+                    # no such file or directory
+                    raise NoSuchFileError(("cannot create %s, path doesn't "
+                                           "exist")  % repr(self.lockname))
+                else:
+                    # re-raise exception in all other cases
+                    raise
+            break
+        self._has_lock= True
+    def __init__(self, filename_, timeout= None):
+        """create a portable lock.
+
+        If timeout is a number, wait up to this time (seconds) to aquire the
+        lock.
+        """
+        # we could disable the locking mechanism here:
+        self._disabled= False
+        if timeout is None:
+            self.timeout= 0
+        else:
+            if not isinstance(timeout, int):
+                raise TypeError("timeout must be None or an int")
+            if timeout<0:
+                raise ValueError("timeout must be >=0")
+            self.timeout= timeout
+
+        self._filename= filename_
+        self.lockname= "%s.lock" % self._filename
+        self._has_lock= False
+        self.info= None
+        if platform.system()=="Linux":
+            self.method= "link"
+        else:
+            self.method= "mkdir"
+    def unlock(self, force= False):
+        """unlock."""
+        if self._disabled:
+            return
+        if not force:
+            if not self._has_lock:
+                raise AssertionError("cannot unlock since a lock "
+                                     "wasn't taken")
+        if self.method=="link":
+            os.unlink(self.lockname)
+        else:
+            for f in os.listdir(self.lockname):
+                os.unlink(os.path.join(self.lockname,f))
+            os.rmdir(self.lockname)
+        self._has_lock= False
+    def filename(self, filename_= None):
+        """gets or sets the name of the file that should be locked."""
+        if filename_ is None:
+            return self._filename
+        if self._has_lock:
+            raise AssertionError("cannot change filename if we already "
+                                 "have a lock")
+        self._filename= filename_
+        self.lockname= "%s.lock" % self._filename
+        return None
+
 def hash_str(data):
     """create an md5 hash of any python data."""
     if isinstance(data, str):
@@ -154,16 +290,38 @@ class Cache:
     def full_cache_filename(self, cache_file):
         """prepend the cache_dir to the filename."""
         return os.path.join(self.cache_dir, cache_file)
-    def read_cache_dict(self):
+    def read_cache_dict(self, lock_file= True):
         """read the cache_dict file."""
-        with open(self.cache_dict_filename) as fh:
-            # may raise IOError, ValueError:
-            self.cache_dict= json.load(fh)
-    def write_cache_dict(self):
+        fn= self.cache_dict_filename
+        if not os.path.exists(self.cache_dict_filename):
+            return
+        mylock= None
+        if lock_file:
+            mylock= MyLock(fn, LOCK_TIMEOUT)
+            # may raise LockedError, AccessError, NoSuchFileError, OSError:
+            mylock.lock()
+        try:
+            with open(fn) as fh:
+                # may raise IOError, ValueError:
+                self.cache_dict= json.load(fh)
+        finally:
+            if mylock:
+                mylock.unlock()
+    def write_cache_dict(self, lock_file= True):
         """write the cache_dict file."""
-        with open(self.cache_dict_filename, "w") as fh:
-            json.dump(self.cache_dict, fh, ensure_ascii= False, sort_keys=
-                      True, indent=4)
+        fn= self.cache_dict_filename
+        mylock= None
+        if lock_file:
+            mylock= MyLock(fn, LOCK_TIMEOUT)
+            # may raise LockedError, AccessError, NoSuchFileError, OSError:
+            mylock.lock()
+        try:
+            with open(fn, "w") as fh:
+                json.dump(self.cache_dict, fh, ensure_ascii= False, sort_keys=
+                          True, indent=4)
+        finally:
+            if lock_file:
+                mylock.unlock()
     def cache_name(self, connection_string, sql):
         """get filename with cache data."""
         connection_data= self.cache_dict.get(connection_string)
@@ -172,31 +330,51 @@ class Cache:
         return connection_data.get(sql)
     def add_cache_name(self, connection_string, sql, filename):
         """add a new filename to self.cache_dict."""
-        connection_data= self.cache_dict.setdefault(connection_string, {})
-        connection_data[sql]= filename
+        fn= self.cache_dict_filename
+        mylock= MyLock(fn, LOCK_TIMEOUT)
+        # may raise LockedError, AccessError, NoSuchFileError, OSError:
+        mylock.lock()
+        try:
+            self.read_cache_dict(lock_file= False)
+            connection_data= self.cache_dict.setdefault(connection_string, {})
+            connection_data[sql]= filename
+            self.write_cache_dict(lock_file= False)
+        finally:
+            mylock.unlock()
     def lookup(self, connection_string, sql):
         """lookup results of an sql statement."""
         cache_name= self.cache_name(connection_string, sql)
         if cache_name is None:
             return None
-        with open(self.full_cache_filename(cache_name)) as fh:
-            result= json.load(fh)
+        fn= self.full_cache_filename(cache_name)
+        mylock= MyLock(fn, LOCK_TIMEOUT)
+        # may raise LockedError, AccessError, NoSuchFileError, OSError:
+        mylock.lock()
+        try:
+            with open(fn) as fh:
+                result= json.load(fh)
+        finally:
+            mylock.unlock()
         return result
     def update(self, connection_string, sql, data):
         """update the cache."""
         cache_name= self.cache_name(connection_string, sql)
-        must_write= False
         if cache_name is None:
             # make new entry
             cache_name= "%s-%s.json" % \
                         (hash_str(connection_string),
                          hash_str(sql))
+            # add_cache_name also writes the cache_dict to disk:
             self.add_cache_name(connection_string, sql, cache_name)
-            must_write= True
-        with open(self.full_cache_filename(cache_name), "w") as fh:
-            json.dump(data, fh, ensure_ascii= False, indent=4)
-        if must_write:
-            self.write_cache_dict()
+        fn= self.full_cache_filename(cache_name)
+        mylock= MyLock(fn, LOCK_TIMEOUT)
+        # may raise LockedError, AccessError, NoSuchFileError, OSError:
+        mylock.lock()
+        try:
+            with open(fn, "w") as fh:
+                json.dump(data, fh, ensure_ascii= False, indent=4)
+        finally:
+            mylock.unlock()
     def cleanup_caches(self):
         """remove caches not referenced in cache_dict."""
         cache_name_set= set()
