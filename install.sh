b857be1440b3e0d29494ec6b208844c5704ae118
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2022-06-27 07:24:58 +0000

    The configuration files are now more generic.
    The configuration file may have one of these names:
    
    - config.FULLHOSTNAME
    - config.DOMAINNAME
    
    If the configuration file for the current host is not found, the
    "install-directory" parameter must be given to the "install.sh" script.

--- install.sh
+++ install.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-HOSTNAME=$(hostname -f)
-
-if [ -z "$1" ]; then
-    if echo $HOSTNAME| grep -E '(elbe|stretch|bullseye)\.acc\.bessy\.de' -q ; then
-        SHORTHOST=$(echo $HOSTNAME | sed -e 's/\..*//')
-        echo "darcs pull is executed in order to prevent"
-        echo "you from installing old program versions by accident"
-        darcs pull
-        # force bii_scripts.config to be remade:
-        rm -f out/script/bii_scripts.config
-        BII_CONFIG=config.$SHORTHOST.acc make -sj install
-        exit
-    fi
-    echo "usage: $0 [install-directory]"
-    exit 
-fi
+FULLHOSTNAME=$(hostname -f)
+HOSTNAME=$(echo $FULLHOSTNAME | sed -e 's/\..*//')
+DOMAINNAME=$(echo $FULLHOSTNAME | sed -e 's/^[^\.]\+\.\?//')
 
 if [ "$1" = "-h" -o "$1" = "--help" ]; then
     echo "usage: $0 [install-directory]"
     exit 
+fi
+
+if [ -z "$1" ]; then
+    CONFIG="config.$FULLHOSTNAME"
+    if [ ! -e "$CONFIG" ]; then
+        if [ -n "$DOMAINNAME" ]; then
+            CONFIG="config.$DOMAINNAME"
+        fi
+    fi
+    if [ ! -e "$CONFIG" ]; then
+        echo "usage: $0 [install-directory]"
+        exit 
+    fi
+    echo "darcs pull is executed in order to prevent"
+    echo "you from installing old program versions by accident"
+    darcs pull
+    # force bii_scripts.config to be remade:
+    rm -f out/script/bii_scripts.config
+    BII_CONFIG="$CONFIG" make -sj install
+    exit
 fi
 
 INSTALLDIR=$1
